9fc92127c4d521ade909c4207a688150550526fc
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-02-22 19:37:31 +0100

    Definizione dell'interfaccia POJO IUserData
    Definita l'interfaccia per gli oggetti utenti del sistema mytalk

---
+++ Sources/DatabaseManagement/Source/org/softwaresynthesis/mytalk/server/abook/IUserData.java
@@ -0,0 +1,152 @@
+package org.softwaresynthesis.mytalk.server.abook;
+
+public interface IUserData 
+{
+	/**
+	 * Ottiene l'identificatore univoco di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @return	l'identificatore univoco di un utente del sistema mytalk
+	 */
+	public Long getId();
+	
+	/**
+	 * Ottiene l'indirizzo e-mail di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @return	l'indirizzo e-mail di un utente del sistema mytalk
+	 */
+	public String getEmail();
+	
+	/**
+	 * Imposta l'indirizzo e-mail di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @param 	mail	indirizzo e-mail di un utente del sistema
+	 * 					mytalk
+	 */
+	public void setEmail(String mail);
+	
+	/**
+	 * Ottiene la password dell'utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @return	password dell'utente del sistema mytalk
+	 */
+	public String getPassword();
+	
+	/**
+	 * Imposta la password di un utente mytalk
+	 * 
+	 * @author 	Andrea Meneghinello
+	 * version	%I%, %G%
+	 * @param 	password	password dell'utente per il sistema mytalk
+	 */
+	public void setPassword(String password);
+	
+	/**
+	 * Ottiene la domanda segreta per il recupero della password di un utente
+	 * del sistema mytalk
+	 * 
+	 * @author 	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @return 	domanda segreta per il recupero della password di un utente
+	 * 			del sistema mytalk
+	 */
+	public String getQuestion();
+	
+	/**
+	 * Imposta la domanda segreta per il recupero della password di un utente
+	 * del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @param 	question	domanda segreta per il recupero della password di
+	 * 						un utente del sistema mytalk
+	 */
+	public void setQuestion(String question);
+	
+	/**
+	 * Ottiene la risposta alla domanda segreta per il recupero della password
+	 * di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @return	risposta alla domanda segreta per il recupero della password
+	 * 			di un utente del sistema mytalk
+	 */
+	public String getAnswer();
+	
+	/**
+	 * Imposta la risposta alla domanda segreta per il recupero della password
+	 * di utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @param 	answer	risposta alla domanda segreta per il recupero della
+	 * 					password di utente del sistema mytalk
+	 */
+	public void setAnswer(String answer);
+	
+	/**
+	 * Ottiene il nome di un utente del sistema mytalk
+	 * 
+	 * @author 	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @return	il nome di un utente del sistema mytalk
+	 */
+	public String getName();
+	
+	/**
+	 * Imposta il nome di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @param 	name	nome di un utente del sistema mytalk
+	 */
+	public void setName(String name);
+	
+	/**
+	 * Ottiene il cognome di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @return	cognome di un utente del sistema mytalk
+	 */
+	public String getSurname();
+	
+	/**
+	 * Imposta il cognome di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @param 	surname	cognome di un utente del sistema mytalk
+	 */
+	public void setSurname(String surname);
+	
+	/**
+	 * Ottiene il percorso, nel server, dove è memorizzata l'immagine
+	 * profilo di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @return	stringa contenente il percorso, nel server, dove è
+	 * 			memorizzata l'immagine di un utente del sistema mytalk
+	 */
+	public String getImage();
+	
+	/**
+	 * Imposta il percorso, nel server, dove sarà memorizzata l'immagine
+	 * profilo di un utente del sistema mytalk
+	 * 
+	 * @author	Andrea Meneghinello
+	 * @version	%I%, %G%
+	 * @param 	path	percorso, nel server, dove sarà memorizzata
+	 * 					l'immagine profilo di un utente del sistema
+	 * 					mytalk
+	 */
+	public void setImage(String path);
+}
