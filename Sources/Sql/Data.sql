500764da94766b5db3cecfa038d04258e0d7cb69
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-02-28 11:08:56 +0100

    Correzione script di installazione del DB
    Aggiunti anche dei dati di test

--- Sources/Sql/Data.sql
+++ Sources/Sql/Data.sql
@@ -1,4 +1,6 @@
 USE mytalk;
 
-INSERT INTO UserData(E_Mail, Password, Question, Answer, Name, Surname)
-VALUES ('indirizzo@dominio.it', 'password', 'Come mi chiamo', 'Pippo', 'Pippo', 'Rossi');
+INSERT INTO UserData(E_Mail, Password, Question, Answer, Name, Surname, Picture)
+VALUES ('indirizzo1@dominio.it', 'password', 'Come mi chiamo', 'Pippo', 'Pippo', 'Rossi', ''),
+VALUES ('indirizzo2@dominio.it', 'password', 'Come mi chiamo', 'Marco', 'Marco', 'Verdi', ''),
+VALUES ('indirizzo3@dominio.it', 'password', 'Come mi chiamo', 'Luigi', 'Luigi', 'Gialli', '');
