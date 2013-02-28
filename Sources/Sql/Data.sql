65f578bc4dfa144adf23104b8e6aa0adb2253237
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-02-28 14:51:07 +0100

    Aggiornato il file di inserimento dati

--- Sources/Sql/Data.sql
+++ Sources/Sql/Data.sql
@@ -3,8 +3,8 @@ USE mytalk;
 INSERT INTO UserData(ID_user, E_Mail, Password, Question, Answer, Name, Surname, Picture)
 VALUES 	(1, 'indirizzo1@dominio.it', 'password', 'Come mi chiamo', 'Pippo', 'Pippo', 'Rossi', ' '),
 		(2, 'indirizzo2@dominio.it', 'password', 'Come mi chiamo', 'Marco', 'Marco', 'Verdi', ' '),
-		(3, 'indirizzo3@dominio.it', 'password', 'Come mi chiamo', 'Luigi', 'Luigi', 'Gialli', ' ');
-		(4, 'indirizzo4@dominio.it'
+		(3, 'indirizzo3@dominio.it', 'password', 'Come mi chiamo', 'Luigi', 'Luigi', 'Gialli', ' '),
+		(4, 'indirizzo4@dominio.it', 'password', 'Come mi chiamo', 'Piero', 'Piero', 'Pelu', ' ');
 		
 INSERT INTO Groups (ID_group, Name)
 VALUES	(1, 'Gruppo 1'),
@@ -15,9 +15,10 @@ VALUES	(1, 'Gruppo 1'),
 		
 INSERT INTO AddressBookEntries (ID_addressBookEntry, ID_user, ID_group, Owner, Blocked)
 VALUES	(1, 2, NULL, 1, 0),
-		(2, 4, 4, 1),
+		(2, 4, 4, 1, 0),
 		(3, 1, NULL, 2, 0),
 		(4, 3, NULL, 2, 0),
 		(5, 4, 2, 2, 0),
 		(6, 1, 3, 3, 0),
-		(7, 2, 3, 3, 0);
+		(7, 2, 3, 3, 0),
+		(8, 2, 2, 3, 0);
