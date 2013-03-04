30cca90a4cd1320cd04da4fdce6049ec947f3678
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-03-04 15:11:38 +0100

    Javadoc per state

--- Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/abook/State.java
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/abook/State.java
@@ -1,5 +1,11 @@
 package org.softwaresynthesis.mytalk.server.abook;
 
+/**
+ * Possibili stati offerti all'utente
+ * del sistema mytalk
+ * @author 	Diego Beraldin
+ * @version %I%, %G%
+ */
 public enum State
 {
 	AVAILABLE,
