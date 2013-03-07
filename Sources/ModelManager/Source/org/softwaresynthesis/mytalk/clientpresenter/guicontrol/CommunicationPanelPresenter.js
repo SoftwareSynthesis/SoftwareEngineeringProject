eb5828213f24cb7c0eb382b3290d1a74a2582717
Riccardo Tresoldi <riccardo.tresoldi@me.com>
2013-03-07 15:46:53 +0100

    Sistemazione metodo initialize() di ComunicationPannelPresenter

--- Sources/ModelManager/Source/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/CommunicationPanelPresenter.js
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/CommunicationPanelPresenter.js
@@ -1,27 +1,32 @@
 /**
- * 
+ *
  * @author Elena Zecchinato
  * @author Diego Beraldin
- * @param mediator  riferimento al mediatore che gestisce la collaborazione fra i presenter
+ * @param mediator riferimento al mediatore che gestisce la collaborazione fra i presenter
  */
 function CommunicationPanelPresenter(mediator) {
-  //FIXME please! Questo ci è stato vietato da ricCARDINo
-  this.mediator = mediator;
-  //elemento controllato da questo presenter
-  this.element = document.getElementById("CommunicationPanel");
-  
-  /**
-   * 
-   * @author Elena Zecchinato
-   */
-  this initialize = function() {
-    //creo i due div principali
-    var divProfile = document.createElement('div')
-    var divChat = document.createElement('div');
-    
-    //apendo il sottoalbero al DOM
-    this.element.innerHTML="";
-    this.element.appendChild(divProfile);
-    this.element.appendChild(divChat);
-     };
+    //FIXME please! Questo ci è stato vietato da ricCARDINo
+    this.mediator = mediator;
+    //elemento controllato da questo presenter
+    this.element = document.getElementById("CommunicationPanel");
+
+    /**
+     *
+     * @author Elena Zecchinato
+     */
+    this
+    initialize = function() {
+        //azzero il div
+        this.element.innerHTML = "";
+        //creo div contenente la chiamata vera e propria
+        var divProfile = document.createElement('div');
+        divProfile.setAttribute("id", "divProfile");
+        //creo div contenente le chat testuali
+        var divChat = document.createElement('div');
+        divProfile.setAttribute("id", "divProfile");
+
+        //apendo il sottoalbero al DOM
+        this.element.appendChild(divProfile);
+        this.element.appendChild(divChat);
+    };
 }
