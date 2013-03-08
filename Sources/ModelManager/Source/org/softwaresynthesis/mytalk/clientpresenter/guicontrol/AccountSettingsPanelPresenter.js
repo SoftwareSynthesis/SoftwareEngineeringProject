545fd66bd4632923495bc7b7f15d353457382c50
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-08 06:51:59 +0100

    aggiunte a AccountSettingsPanelPresenter

--- Sources/ModelManager/Source/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/AccountSettingsPanelPresenter.js
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/AccountSettingsPanelPresenter.js
@@ -1,3 +1,5 @@
+//TRES QUI CI STO ANCORA LAVORANDO IO
+
 /**
  * Presenter incaricato di gestire il pannello delle impostazioni dell'utente
  * 
@@ -7,43 +9,96 @@
 function AccountSettingsPanelPresenter() {
     //elemento controllato da questo presenter
     this.element = document.getElementById("AccountSettingsPanel");
+    //url della servlet con cui il presenter deve comunicare
+    this.servletURL = "http://localhost:8080/AccountManager";
 
     /**
      * Inizializza il pannello costruendone i widget grafici interni
      * 
      * @author Elena Zecchinato
      */
-    this.initialize = function() {
-        var ulData = document.createElement('ul');
-        
+    this.initialize = function() {  
         /* Tutti gli elementi qui mostrati sono recuperati tramite
          * il riferimento al communicationcenter e la proprietà my in
          * esso contenuta, dove sono memorizzati i dati relativi all'utente.
          */
-        var name = document.createElement('li');
-        name.innerHTML = communicationcenter.my.name;
+        var nameNode = document.createElement('li');
+        nameNode.setAttribute("id", "name");
+        nameNode.innerHTML = communicationcenter.my.name;
         
-        var surname = document.createElement('li');
-        surname.innerHTML = communicationcenter.my.surname;
+        var surnameNode = document.createElement('li');
+        nameNode.setAttribute("id", "surname");
+        surnameNode.innerHTML = communicationcenter.my.surname;
         
-        var mail = document.createElement('li');
-        mail.innerHTML = communicationcenter.my.email;
+        var mailNode = document.createElement('li');
+        mailNode.setAttribute("id", "mail");
+        mailNode.innerHTML = communicationcenter.my.email;
 
-        var picure = document.createElement('img');
-        picture.setAttribute("src", communicationcenter.my.picture);
+        var picureNode = document.createElement('img');
+        pictureNode.setAttribute("id", "picture");
+        pictureNode.setAttribute("src", communicationcenter.my.picture);
 
-        var change = document.createElement('button');
-        change.setAttribute("type", "button");
-        //TODO attaccare comportamento al bottone 'change'
+        var changeButton = document.createElement('button');
+        changeButton.setAttribute("type", "button");
+        changeButton.innerHTML = "Modifica dati";
+        changeButton.onclick = this.onChange;
         
         //appende i sottonodi ai nodi principali
-        ulData.appendChild(name);
-        ulData.appendChild(surname);
-        ulData.appendChild(mail);
+        var ulData = document.createElement('ul');
+        ulData.style.listStyleType = "none";
+        ulData.appendChild(nameNode);
+        ulData.appendChild(surnameNode);
+        ulData.appendChild(mailNode);
 
         //appende il sottoalbero al DOM
-        this.element.appendChild(picture);
+        this.element.appendChild(pictureNode);
         this.element.appendChild(ulData);
-        this.element.appendChild(change);
+        this.element.appendChild(changeButton);
+    };
+    
+    /**
+     * Verifica che sia effettivamente cambiato qualcosa rispetto a quanto contenuto
+     * in communicationcenter.my nell'oggetto data che viene passato come paramtro
+     * 
+     * @author Diego Beraldin
+     */
+    this.hasSomethingChanged = function(data) {
+    	var my = communicationcenter.my;
+    	if (my.name != data.name || my.surname != data.surname || my.picture != data.picture) {
+    		return true;
+    	}
+    	return false;
+    };
+    
+    /**
+     * Gestisce la pressione del pulsante 'changeButton' che trasforma la lista di
+     * elementi testuali in un form da compilare per modificare i propri dati.
+     * Il form è completo di un pulsante submit che attiva 'onSubmitChange'
+     * 
+     * @author Diego Beraldin
+     */
+    this.onChange = function() {
+    	
+    };
+    
+    /**
+     * Gestisce il cambiamento dei dati da parte dell'utente contattando, se necessario
+     * la servlet incaricata di aggiornare il contenuto del database sul server
+     * 
+     * @author Diego Beraldin
+     */
+    this.onSubmitChange = function() {
+    	// recupera i dati dal form e li memorizza in un oggetto
+    	var data = new Object();
+    	var data.name = document.getElementById("name").innerHTML;
+    	var data.surname = document.getElementById("surname").innerHTML;
+    	var data.mail = document.getElementById("mail").innerHTML;
+    	//FIXME bisogna far cambiare l'immagine del profilo
+    	var data.picture = "";
+    	
+    	// verifica se è cambiato qualcosa e agisce di conseguenza
+    	if (this.hasSomethingChanged(data)) {
+    		//TODO contattare la servlet con una richiesta AJAX
+    	}
     };
 }
