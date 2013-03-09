1bb766dfbc385f182e3c0105299afcdb489389bd
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-09 16:10:12 +0100

    riorganizzate AccountSettingsPP in stile OO

--- Sources/ModelManager/WebContent/clientpresenter/guicontrol/AccountSettingsPanelPresenter.js
+++ Sources/ModelManager/WebContent/clientpresenter/guicontrol/AccountSettingsPanelPresenter.js
@@ -1,5 +1,3 @@
-//TRES QUI CI STO ANCORA LAVORANDO IO
-
 /**
  * Presenter incaricato di gestire il pannello delle impostazioni dell'utente
  * 
@@ -9,17 +7,73 @@
  * @author Diego Beraldin
  */
 function AccountSettingsPanelPresenter() {
+/**********************************************************
+                      VARIABILI PRIVATE
+***********************************************************/
     //elemento controllato da questo presenter
-    this.element = document.getElementById("AccountSettingsPanel");
+    var element = document.getElementById("AccountSettingsPanel");
     //url della servlet con cui il presenter deve comunicare
-    this.servletURL = "http://localhost:8080/AccountManager";
+    var servletURL = "http://localhost:8080/AccountManager";
 
+/**********************************************************
+                      METODI PRIVATI
+***********************************************************/
+    /**
+     * Verifica che sia effettivamente cambiato qualcosa rispetto a quanto contenuto
+     * in communicationcenter.my nell'oggetto data che viene passato come paramtro
+     * 
+     * @author Diego Beraldin
+     */
+    function hasSomethingChanged(data) {
+    	var my = communicationcenter.my;
+    	if (my.name != data.name || my.surname != data.surname || my.picture != data.picture) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    /**
+     * Gestisce la pressione del pulsante 'changeButton' che trasforma la lista di
+     * elementi testuali in un form da compilare per modificare i propri dati.
+     * Il form è completo di un pulsante submit che attiva 'onSubmitChange'
+     * 
+     * @author Diego Beraldin
+     */
+    function onChangeButtonPressed() {
+    	//TODO da terminare!
+    }
+    
+    /**
+     * Gestisce il cambiamento dei dati da parte dell'utente contattando, se necessario
+     * la servlet incaricata di aggiornare il contenuto del database sul server
+     * 
+     * @author Diego Beraldin
+     */
+    function onSubmitChange() {
+    	// recupera i dati dal form e li memorizza in un oggetto
+    	var data = new Object();
+    	data.name = document.getElementById("name").innerHTML;
+    	data.surname = document.getElementById("surname").innerHTML;
+    	data.mail = document.getElementById("mail").innerHTML;
+    	// cosa fare con l'immagine del profilo
+    	data.picture = "";
+    	
+    	// verifica se è cambiato qualcosa e agisce di conseguenza
+    	if (this.hasSomethingChanged(data)) {
+    		//TODO contattare la servlet con una richiesta AJAX
+    	}
+    }
+    
+/**********************************************************
+                      METODI PUBBLICI
+***********************************************************/
     /**
      * Inizializza il pannello costruendone i widget grafici interni
      * 
      * @author Elena Zecchinato
      */
     this.initialize = function() {  
+    	element.style.display = "block";
         /* Tutti gli elementi qui mostrati sono recuperati tramite
          * il riferimento al communicationcenter e la proprietà my in
          * esso contenuta, dove sono memorizzati i dati relativi all'utente.
@@ -43,10 +97,7 @@ function AccountSettingsPanelPresenter() {
         var changeButton = document.createElement('button');
         changeButton.setAttribute("type", "button");
         changeButton.innerHTML = "Modifica dati";
-        var self = this;
-        changeButton.onclick = function() {
-        	self.onChange();
-        };
+        changeButton.onclick = onChangeButtonPressed;
         
         //appende i sottonodi ai nodi principali
         var ulData = document.createElement('ul');
@@ -56,54 +107,17 @@ function AccountSettingsPanelPresenter() {
         ulData.appendChild(mailNode);
 
         //appende il sottoalbero al DOM
-        this.element.appendChild(pictureNode);
-        this.element.appendChild(ulData);
-        this.element.appendChild(changeButton);
+        element.appendChild(pictureNode);
+        element.appendChild(ulData);
+        element.appendChild(changeButton);
     };
     
     /**
-     * Verifica che sia effettivamente cambiato qualcosa rispetto a quanto contenuto
-     * in communicationcenter.my nell'oggetto data che viene passato come paramtro
+     * Rende invisibile il pannello delle impostazioni
      * 
      * @author Diego Beraldin
      */
-    this.hasSomethingChanged = function(data) {
-    	var my = communicationcenter.my;
-    	if (my.name != data.name || my.surname != data.surname || my.picture != data.picture) {
-    		return true;
-    	}
-    	return false;
-    };
-    
-    /**
-     * Gestisce la pressione del pulsante 'changeButton' che trasforma la lista di
-     * elementi testuali in un form da compilare per modificare i propri dati.
-     * Il form è completo di un pulsante submit che attiva 'onSubmitChange'
-     * 
-     * @author Diego Beraldin
-     */
-    this.onChange = function() {
-    	
-    };
-    
-    /**
-     * Gestisce il cambiamento dei dati da parte dell'utente contattando, se necessario
-     * la servlet incaricata di aggiornare il contenuto del database sul server
-     * 
-     * @author Diego Beraldin
-     */
-    this.onSubmitChange = function() {
-    	// recupera i dati dal form e li memorizza in un oggetto
-    	var data = new Object();
-    	data.name = document.getElementById("name").innerHTML;
-    	data.surname = document.getElementById("surname").innerHTML;
-    	data.mail = document.getElementById("mail").innerHTML;
-    	// cosa fare con l'immagine del profilo
-    	data.picture = "";
-    	
-    	// verifica se è cambiato qualcosa e agisce di conseguenza
-    	if (this.hasSomethingChanged(data)) {
-    		//TODO contattare la servlet con una richiesta AJAX
-    	}
+    this.hide = function() {
+    	element.style.display = "none";
     };
 }
