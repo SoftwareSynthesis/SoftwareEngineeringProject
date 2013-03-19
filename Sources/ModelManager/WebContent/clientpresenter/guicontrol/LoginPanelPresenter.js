0562009057072c34c7aa5bf8daf2ec80dd427ce9
Riccardo Tresoldi <riccardo.tresoldi@me.com>
2013-03-19 12:55:00 +0100

    UPDATE LoginPP

--- Sources/ModelManager/WebContent/clientpresenter/guicontrol/LoginPanelPresenter.js
+++ Sources/ModelManager/WebContent/clientpresenter/guicontrol/LoginPanelPresenter.js
@@ -8,18 +8,32 @@
  */
 function LoginPanelPresenter(url) {
     /**********************************************************
-    VARIABILI PRIVATE
-    ***********************************************************/
+     VARIABILI PRIVATE
+     ***********************************************************/
     // url della servlet che deve gestire il login
     var servletURL = url;
+    var operation = new Array();
+    operation.push("Autentication");
+    operation.push("Close");
+    operation.push("GetSecretQuestion");
+    operation.push("DoSecretAnswer");
+    operation.push("DoRegistration");
+    /*
+    * 0 = LoginAutentication
+    * 1 = LoginClose
+    * 2 = LoginGetSecretQuestion
+    * 3 = LoginDoSecretAnswer
+    * 4 = LoginDoRegistration
+    */
+
     // elemento controllato da questo presenter
     var element = document.getElementById("LoginPanel");
     // questo è leggermente esoterico
     var self = this;
-    
+
     /**********************************************************
-    METODI PRIVATI
-    ***********************************************************/
+     METODI PRIVATI
+     ***********************************************************/
     /**
      * Testa quanto ricevuto dal server e, in caso di login avvenuto correttamente
      * reindirizza il browser nella pagina finale dopo aver salvato i dati dell'utente
@@ -35,55 +49,58 @@ function LoginPanelPresenter(url) {
             mediator.buildUI();
         }
     }
-    
+
     /**
      * Rimuove il form di recupero della password, visualizza un
      * messaggio di conferma per 2 secondi quindi rimuove anche
      * questo e lascia il controllo al form di login.
-     * 
+     *
      * @author Diego Beraldin
      */
     function correctAnswer() {
-    	var formRetrievePassword = document.getElementById("passwordretrieval");
-    	element.removeChild(formRetrievePassword);
-    	var message = document.createElement("p");
-    	var text = document.createTextNode("Recupero password avvenuto correttamente." +
-    			"Ti è stata inviata un'email contenente i dati richiesti.");
-    	message.appendChild(text);
-    	element.appendChild(message);
-    	window.setTimeout(function(){ element.removeChild(message); }, 2000);
+        var formRetrievePassword = document.getElementById("passwordretrieval");
+        element.removeChild(formRetrievePassword);
+        var message = document.createElement("p");
+        var text = document.createTextNode("Recupero password avvenuto correttamente." + "Ti è stata inviata un'email contenente i dati richiesti.");
+        message.appendChild(text);
+        element.appendChild(message);
+        window.setTimeout(function() {
+            element.removeChild(message);
+        }, 2000);
     }
-    
+
     /**
      * In caso di inserimento della risposta non corretta alla domanda segreta
      * visualizza un messaggio di avvertimento all'utente per 2 secondi quindi lascia il
      * controllo al form di inserimento della risposta alla domanda segreta.
-     * 
+     *
      * @author Diego Beraldin
      */
     function incorrectAnswer() {
-    	var message = document.createElement("p");
-    	var text = document.createTextNode("Dati non corretti. Inserire nuovamente la risposta.");
-    	message.appendChild(text);
-    	element.appendChild(message);
-    	window.setTimeout(function(){ element.removeChild(message); }, 2000);
+        var message = document.createElement("p");
+        var text = document.createTextNode("Dati non corretti. Inserire nuovamente la risposta.");
+        message.appendChild(text);
+        element.appendChild(message);
+        window.setTimeout(function() {
+            element.removeChild(message);
+        }, 2000);
     }
-    
+
     /**
      * Recupera la domanda segreta con una richiesta asincrona al server
-     * 
+     *
      * @returns {String} la domanda segreta impostata dall'utente
      * @author Diego Beraldin
      */
     function getSecretQuestion() {
-    	var userID = self.getUsername();
-    	var question = "";
-    	var request = new XMLHttpRequest();
-    	request.open("POST", servletURL, false);
-    	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    	request.send("username=" + encodeURIComponent(userID) + "&operation=3");
-    	question = request.responseText;
-    	return question;
+        var userID = self.getUsername();
+        var question = "";
+        var request = new XMLHttpRequest();
+        request.open("POST", servletURL + operation[2], false);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send("username=" + encodeURIComponent(userID) + "&operation=3");
+        question = request.responseText;
+        return question;
     }
 
     /**********************************************************
@@ -91,109 +108,108 @@ function LoginPanelPresenter(url) {
      ***********************************************************/
     /**
      * Testa se l'utente ha dato la risposta corretta alla domanda segreta
-     * 
+     *
      * @param {String} username nome utente associato alla domanda segreta
      * @param {String} answer risposta alla domanda segreta
      * @returns {Boolean} true se la risposta è corretta, false altrimenti
      * @author Diego Beraldin
      */
     this.hasAnsweredCorrectly = function(username, answer) {
-    	var request = new XMLHttpRequest();
-    	request.open("POST", servletURL, false);
-    	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    	request.send("username=" + encodeURIComponent(username) + "&answer=" + encodeURIComponent(answer) + "&operation=4");
-    	return JSON.parse(request.responseText);
+        var request = new XMLHttpRequest();
+        request.open("POST", servletURL, false);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send("username=" + encodeURIComponent(username) + "&answer=" + encodeURIComponent(answer) + "&operation=4");
+        return JSON.parse(request.responseText);
     };
-    
+
     /**
      * Costruisce il form per il recupero della password dell'utente
-     * 
+     *
      * @param {String} username l'email dell'utente
      * @returns {HTMLFormElement} il form per il recupero della password
      * @author Diego Beraldin
      */
     this.buildRetrievePasswordForm = function() {
-    	// costruisce il form con il valore ottenuto
-    	var formRetrievePassword = document.createElement("fieldset");
-    	formRetrievePassword.setAttribute("id", "passwordretrieval");
-    	// etichetta con la domanda
-    	var labelQuestion = document.createElement("label");
-    	labelQuestion.setAttribute("for", "inputanswer");
-    	// procura il testo della domanda segreta
-    	var text = getSecretQuestion();
-    	var question = document.createTextNode(text);
-    	labelQuestion.appendChild(question);
-    	// campo di immissione
-    	var inputAnswer = document.createElement("input");
-    	inputAnswer.setAttribute("id", "inputanswer");
-    	inputAnswer.setAttribute("name", "inputanswer");
-    	inputAnswer.setAttribute("placeholder", "risposta");
-    	inputAnswer.setAttribute("required", "required");
-    	// pulsante di conferma
-    	var submitButton = document.createElement("input");
-    	submitButton.setAttribute("type", "submit");
-    	submitButton.setAttribute("value", "OK");
-    	submitButton.onClick = function() {
-    		//Answer(username, inputAnswer.getAttribute("value"));
-    		if (this.hasAnsweredCorrectly(username, inputAnswer.getAttribute("value"))) {
-    			correctAnswer();
-    		} else {
-    			incorrectAnswer();
-    		}
-    	};
-    	
-    	formRetrievePassword.appendChild(labelQuestion);
-    	formRetrievePassword.appendChild(inputAnswer);
-    	formRetrievePassword.appendChild(submitButton);
-    	return formRetrievePassword;
+        // costruisce il form con il valore ottenuto
+        var formRetrievePassword = document.createElement("fieldset");
+        formRetrievePassword.setAttribute("id", "passwordretrieval");
+        // etichetta con la domanda
+        var labelQuestion = document.createElement("label");
+        labelQuestion.setAttribute("for", "inputanswer");
+        // procura il testo della domanda segreta
+        var text = getSecretQuestion();
+        var question = document.createTextNode(text);
+        labelQuestion.appendChild(question);
+        // campo di immissione
+        var inputAnswer = document.createElement("input");
+        inputAnswer.setAttribute("id", "inputanswer");
+        inputAnswer.setAttribute("name", "inputanswer");
+        inputAnswer.setAttribute("placeholder", "risposta");
+        inputAnswer.setAttribute("required", "required");
+        // pulsante di conferma
+        var submitButton = document.createElement("input");
+        submitButton.setAttribute("type", "submit");
+        submitButton.setAttribute("value", "OK");
+        submitButton.onClick = function() {
+            //Answer(username, inputAnswer.getAttribute("value"));
+            if (this.hasAnsweredCorrectly(username, inputAnswer.getAttribute("value"))) {
+                correctAnswer();
+            } else {
+                incorrectAnswer();
+            }
+        };
+
+        formRetrievePassword.appendChild(labelQuestion);
+        formRetrievePassword.appendChild(inputAnswer);
+        formRetrievePassword.appendChild(submitButton);
+        return formRetrievePassword;
     };
-    
+
     /**
      * Recupera lo username dall'interfaccia grafica utente
-     * 
+     *
      * NOTA PER I VERIFICATORI
      * Richiede che il 'document' abbia al suo interno un elemento di
      * tipo '<input>' con 'id' uguale a 'username' contenente l'indirizzo
      * email dell'utente che intende autenticarsi al sistema
-     * 
+     *
      * @author Diego Beraldin
      * @returns {String} lo username dell'utente
      */
     this.getUsername = function() {
-    	var username = document.getElementById("username").value;
-    	if (!username || username.length == 0) {
-    		throw "valore non specificato";
-    	}
-    	var emailRegex = new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$", "i");
-    	if (!emailRegex.test(username)) {
-    		throw "indirizzo email non valido";
-    	}
-    	return username;
+        var username = document.getElementById("username").value;
+        if (!username || username.length == 0) {
+            throw "valore non specificato";
+        }
+        var emailRegex = new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$", "i");
+        if (!emailRegex.test(username)) {
+            throw "indirizzo email non valido";
+        }
+        return username;
     };
-    
+
     /**
      * Recupera lo username dall'interfaccia grafica utente
-     * 
+     *
      * NOTA PER I VERIFICATORI
      * Richiede che il 'document' abbia al suo interno un elemento di
      * tipo '<input>' con 'id' uguale a 'password' contenente la password
      * dell'utente che intende autenticarsi al sistema
-     * 
+     *
      * @author Diego Beraldin
      * @returns {String} lo username dell'utente
      */
     this.getPassword = function() {
-    	var password = document.getElementById("password").value;
-    	if (!password || password.length == 0) {
-    		throw "valore non specificato";
-    	}
-    	return password;
+        var password = document.getElementById("password").value;
+        if (!password || password.length == 0) {
+            throw "valore non specificato";
+        }
+        return password;
     };
-    
-    
+
     /**
      * Procedura che esegue il login inviando al server i dati di autenticazione
-     * 
+     *
      * NOTE PER I VERIFICATORI
      * Testare communicationcenter.my al termine della procedura di login!!!
      *
@@ -211,28 +227,27 @@ function LoginPanelPresenter(url) {
         };
         request.open("POST", servletURL, true);
         request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        var querystring = "username=" + encodeURIComponent(data.username) +
-        				  "&password=" + encodeURIComponent(data.password) + "&operation=1";
+        var querystring = "username=" + encodeURIComponent(data.username) + "&password=" + encodeURIComponent(data.password) + "&operation=1";
         request.send(querystring);
         return querystring;
     };
-    
+
     /**
      * Nasconde il form di autenticazione per lasciare spazio nella finestra a
      * altri elementi grafici come la schermata principale o il pannello di registrazione
-     * 
+     *
      * @author Diego Beraldin
      */
     this.hide = function() {
-    	if (element) {
-    	    element.style.display = "none";
-    	}
+        if (element) {
+            element.style.display = "none";
+        }
     };
 
     /**
      * Inizializzazione del pannello di login con la creazione di tutti i
      * widget grafici che sono contenuti al suo interno
-     * 
+     *
      * NOTA PER I VERIFICATORI:
      * Richiede che nel 'document' sia presente un elemento '<div>' che
      * abbia come attributo 'id' il valore 'LoginPanel'
@@ -240,10 +255,10 @@ function LoginPanelPresenter(url) {
      * @author Diego Beraldin
      */
     this.initialize = function() {
-    	element.style.display = "block";
+        element.style.display = "block";
         //creazione dell'elemento form
         var loginForm = document.createElement('fieldset');
-        loginForm.id="loginForm";
+        loginForm.id = "loginForm";
 
         //creazione dell'elemento <ul> contenuto nel form
         var ulData = document.createElement('ul');
@@ -284,33 +299,32 @@ function LoginPanelPresenter(url) {
         liPassword.appendChild(labelPassword);
         liPassword.appendChild(inputPassword);
 
-
         //pulsante di login
         var inputLogin = document.createElement('button');
-        inputLogin.type="button";
+        inputLogin.type = "button";
         inputLogin.appendChild(document.createTextNode('Login'));
         var self = this;
         inputLogin.onclick = function() {
-        	var data = new Object();
-        	data.username = self.getUsername();
-        	data.password = self.getPassword();
-        	self.login(data);
+            var data = new Object();
+            data.username = self.getUsername();
+            data.password = self.getPassword();
+            self.login(data);
         };
         //pulsante di registrazione
         var inputRegister = document.createElement('button');
         inputRegister.type = "button";
         inputRegister.appendChild(document.createTextNode('Registrazione'));
         inputRegister.onclick = function() {
-        	self.hide();
+            self.hide();
             mediator.buildRegistrationUI();
         };
         //pulsante per recuperare la password
         var inputRetrievePassword = document.createElement("button");
-        inputRetrievePassword.type="button";
+        inputRetrievePassword.type = "button";
         inputRetrievePassword.appendChild(document.createTextNode('Recupera password'));
         inputRetrievePassword.onclick = function() {
-        	var form = self.buildRetrievePasswordForm();
-        	element.appendChild(form);
+            var form = self.buildRetrievePasswordForm();
+            element.appendChild(form);
         };
 
         //creazione dell'item per i pulsanti
