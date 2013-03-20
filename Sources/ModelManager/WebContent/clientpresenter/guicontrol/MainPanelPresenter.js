56d72067dc7e53c7e901be4136bd6aaf85f38be5
Riccardo Tresoldi <riccardo.tresoldi@me.com>
2013-03-20 09:56:21 +0100

    Migliorata formattazione mainPP

--- Sources/ModelManager/WebContent/clientpresenter/guicontrol/MainPanelPresenter.js
+++ Sources/ModelManager/WebContent/clientpresenter/guicontrol/MainPanelPresenter.js
@@ -7,48 +7,50 @@
  * @author Diego Beraldin
  */
 function MainPanelPresenter() {
-	/**********************************************************
-    VARIABILI PRIVATE
-    ***********************************************************/
+    /**********************************************************
+     VARIABILI PRIVATE
+     ***********************************************************/
     //elemento controllato da questo presenter
     var element = document.getElementById("MainPanel");
-    
-	/**********************************************************
-    METODI PUBBICI
-    ***********************************************************/
+
+    /**********************************************************
+     METODI PUBBICI
+     ***********************************************************/
     /**
      * Costruisce il pannello principale dell'applicazione che occupa il posto
      * centrale della finestra
-     * 
+     *
      * @author Elena Zecchinato
      */
     this.initialize = function() {
-    	element.style.display = "block";
+        element.style.display = "block";
         element.innerHTML = "";
+        
         //immagine di spondo per quando il pannello è vuoto
         var img = document.createElement("img");
-        img.setAttribute("src", "img/mytalk.png");
+        img.src = "img/mytalk.png";
 
         this.element.appendChild(img);
     };
-    
+
     /**
      * Visualizza un elemento interno al pannello principale
-     * 
-     * @param {HTMLDivElement} nodo che deve essere visualizzato all'interno del pannello principale
+     *
+     * @param {HTMLDivElement} nodo che deve essere visualizzato all'interno del
+     * pannello principale
      * @author Diego Beraldin
      */
     this.displayChildPanel = function(node) {
-    	element.innerHTML = "";
-    	element.appendChild(node);
+        element.innerHTML = "";
+        element.appendChild(node);
     };
-    
+
     /**
      * Rende invisibile il pannello principale
-     * 
+     *
      * @author Diego Beraldin
      */
     this.hide = function() {
-    	element.style.display = "none";
+        element.style.display = "none";
     };
 }
