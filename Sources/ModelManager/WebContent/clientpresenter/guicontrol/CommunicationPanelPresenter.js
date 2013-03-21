edf126a44b56ce49d96c5945bc65bd585c58c5fc
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-21 18:01:15 +0100

    modificato e corretto AddressBookPP e relativo test

--- Sources/ModelManager/WebContent/clientpresenter/guicontrol/CommunicationPanelPresenter.js
+++ Sources/ModelManager/WebContent/clientpresenter/guicontrol/CommunicationPanelPresenter.js
@@ -16,13 +16,57 @@ function CommunicationPanelPresenter() {
     /**********************************************************
     METODI PRIVATI
     ***********************************************************/
+	/**
+	 * 
+	 */
 	function populateChatUl(ul) {
-		//TODO da terminare questa funzione
+		for (var chat in communicationcenter.openChat) {
+			var item = document.createElement("li");
+			// costruisce il list item
+			var user = chat.user;
+			item.id = user.id;
+			var title = "";
+	        if (user.name != null) {
+	        	title += user.name;
+	        }
+	        if (user.surname != null) {
+	            if (title != "") {
+	            	title += " ";
+	            }
+	            title += user.surname;
+	        }
+	        if (title == "") {
+	        	title += user.email;
+	        }
+			item.appendChild(document.createTextNode(title));
+			item.onclick = function() {
+				// operazione da svolgere al click su un list item
+				var divChat = document.getElementById("divChat");
+				var  divContainerChat = document.getElementById("divContainerChat");
+				divChat.removeChild(divContainerChat);
+				divChat.appendChild(chatElements[user.id]);
+			};
+			ul.appendChild(item);
+		}
 	}
 	
     /**********************************************************
      METODI PUBBLICI
      ***********************************************************/
+	
+	/**
+	 * Aggiunge una stringa all'interno dell'area di testo che è 
+	 * associata alla chat con l'utente passato come parametro
+	 * 
+	 * @param {Object} utente con cui è avviata la chat
+	 * @param {String} text testo da appendere al termine della textarea
+	 * @author Diego Beraldin
+	 */
+	this.appendToChat = function(user, text) {
+		var divContainerChat = chatElements[user.id];
+		divContainerChat.childNodes[0].value += ("io:" + text + "\n");
+	};
+	
     /**
      * Inizializza il pannello costruendone i widget grafici interni e lo
      * restituisce in modo che possa essere inserito all'interno del pannello principale
@@ -68,15 +112,33 @@ function CommunicationPanelPresenter() {
         //creo l'<ul> per le schede delle chat aperte
         var ulOpenChat = document.createElement('ul');
         ulOpenChat.id = "ulOpenChat";
+        
+        //crea gli HTMLDivElement con il testo della chat
+		for (var chat in communicationCenter.openChat) {
+			var element = document.createElement("div");
+			var form = document.createElement("form");
+			element.id = "divContainerChat";
+			var textArea = document.createElement("textarea");
+			element.appendChild(textArea);
+			
+			var input = document.createElement("input");
+			input.id = "text";
+			input.name = "text";
+			
+			// appende l'elemento all'array associativo chatElements
+			chatElements[chat.id] = element;
+		}
+		
+        //popola la lista delle chat
         populateChatUl(ulOpenChat);
         
         //creo il div per la visualizzazione della chat selezionata.
-        var divContainerChat = document.createElement('div');
-        divContainerChat.id = "divContainerChat";
+//        var divContainerChat = document.createElement('div');
+//        divContainerChat.id = "divContainerChat";
 
-        //appendo i child alla divChat
+        //appendo la lista  al <div> della chat
         divChat.appendChild(ulOpenChat);
-        divChat.appendChild(divContainerChat);
+//        divChat.appendChild(divContainerChat);
 
         //apendo il sottoalbero al DOM
         element.appendChild(divCall);
