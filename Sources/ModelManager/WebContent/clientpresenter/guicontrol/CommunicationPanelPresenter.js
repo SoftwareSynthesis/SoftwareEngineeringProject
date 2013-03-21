250d28172f8bdce0afecf5bd66a75eb0a950e036
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-21 18:11:02 +0100

    aggiunte a CommunicationPanelPresenter

--- Sources/ModelManager/WebContent/clientpresenter/guicontrol/CommunicationPanelPresenter.js
+++ Sources/ModelManager/WebContent/clientpresenter/guicontrol/CommunicationPanelPresenter.js
@@ -1,152 +1,182 @@
 /**
- * Presenter incaricato di gestire il pannello delle comunicazioni,
- * siano esse di natura testuale oppure di natura audio o audio/video
- *
+ * Presenter incaricato di gestire il pannello delle comunicazioni, siano esse
+ * di natura testuale oppure di natura audio o audio/video
+ * 
  * @author Elena Zecchinato
  * @author Diego Beraldin
  */
 function CommunicationPanelPresenter() {
-    /**********************************************************
-    VARIABILI PRIVATE
-    ***********************************************************/
+	/***************************************************************************
+	 * VARIABILI PRIVATE
+	 **************************************************************************/
 	// array associativo di tutte le chat che ho aperte in un dato momento
-    // gli elementi memorizzati in questo array sono {HTMLDivElements}
+	// gli elementi memorizzati in questo array sono {HTMLDivElements}
 	var chatElements = new Object();
-	
-    /**********************************************************
-    METODI PRIVATI
-    ***********************************************************/
+
+	/***************************************************************************
+	 * METODI PRIVATI
+	 **************************************************************************/
+	/**
+	 * @returns {String} il testo che deve costituire il titolo del list item
+	 * @author Diego Beraldin
+	 */
+	function createLabel(user) {
+		var title = "";
+		if (user.name != null) {
+			title += user.name;
+		}
+		if (user.surname != null) {
+			if (title != "") {
+				title += " ";
+			}
+			title += user.surname;
+		}
+		if (title == "") {
+			title += user.email;
+		}
+		return title;
+	}
+
 	/**
 	 * 
+	 * @param {HTMLUListElement}
+	 *            ul lista che deve essere popolata con i nomi delle chat attive
+	 * @author Diego Beraldin
 	 */
 	function populateChatUl(ul) {
-		for (var chat in communicationcenter.openChat) {
+		for ( var chat in communicationcenter.openChat) {
 			var item = document.createElement("li");
 			// costruisce il list item
 			var user = chat.user;
 			item.id = user.id;
-			var title = "";
-	        if (user.name != null) {
-	        	title += user.name;
-	        }
-	        if (user.surname != null) {
-	            if (title != "") {
-	            	title += " ";
-	            }
-	            title += user.surname;
-	        }
-	        if (title == "") {
-	        	title += user.email;
-	        }
-			item.appendChild(document.createTextNode(title));
+
+			item.appendChild(document.createTextNode(createLabel(user)));
 			item.onclick = function() {
 				// operazione da svolgere al click su un list item
 				var divChat = document.getElementById("divChat");
-				var  divContainerChat = document.getElementById("divContainerChat");
+				var divContainerChat = document
+						.getElementById("divContainerChat");
 				divChat.removeChild(divContainerChat);
 				divChat.appendChild(chatElements[user.id]);
 			};
 			ul.appendChild(item);
 		}
 	}
-	
-    /**********************************************************
-     METODI PUBBLICI
-     ***********************************************************/
-	
+
 	/**
-	 * Aggiunge una stringa all'interno dell'area di testo che è 
-	 * associata alla chat con l'utente passato come parametro
 	 * 
-	 * @param {Object} utente con cui è avviata la chat
-	 * @param {String} text testo da appendere al termine della textarea
+	 * @param {Object}
+	 *            chat oggetto che rappresenta una chat nel formato in cui sono
+	 *            memorizzate nel CommunicationCenter. In particolare, ogni chat
+	 *            è dotata di una proprietà 'user' che corrisponde all'utente
+	 *            con cui la comunicazione è instaurata
+	 * @returns
+	 */
+	function createChatElement(chat) {
+		var element = document.createElement("div");
+		element.id = "divContainerChat";
+		var form = document.createElement("form");
+		var textArea = document.createElement("textarea");
+		form.appendChild(textArea);
+
+		var input = document.createElement("input");
+		input.id = "text";
+		input.name = "text";
+		form.appendChild(input);
+		// TODO creare il pulsante
+
+		element.appendChild(form);
+		return element;
+	}
+
+	/***************************************************************************
+	 * METODI PUBBLICI
+	 **************************************************************************/
+
+	/**
+	 * Aggiunge una stringa all'interno dell'area di testo che è associata alla
+	 * chat con l'utente passato come parametro
+	 * 
+	 * @param {Object}
+	 *            utente con cui è avviata la chat
+	 * @param {String}
+	 *            text testo da appendere al termine della textarea
 	 * @author Diego Beraldin
 	 */
 	this.appendToChat = function(user, text) {
 		var divContainerChat = chatElements[user.id];
 		divContainerChat.childNodes[0].value += ("io:" + text + "\n");
 	};
-	
-    /**
-     * Inizializza il pannello costruendone i widget grafici interni e lo
-     * restituisce in modo che possa essere inserito all'interno del pannello principale
-     *
-     * @returns {HTMLDivElement} il 'CommunicationPanel'
-     * @author Elena Zecchinato
-     * @author Riccardo Tresoldi
-     */
-    this.createPanel = function() {
-        var element = document.createElement("div");
-        element.setAttribute("id", "CommunicationPanel");
 
-        //azzero il div
-        element.innerHTML = "";
+	/**
+	 * Inizializza il pannello costruendone i widget grafici interni e lo
+	 * restituisce in modo che possa essere inserito all'interno del pannello
+	 * principale
+	 * 
+	 * @returns {HTMLDivElement} il 'CommunicationPanel'
+	 * @author Elena Zecchinato
+	 * @author Riccardo Tresoldi
+	 */
+	this.createPanel = function() {
+		var element = document.createElement("div");
+		element.setAttribute("id", "CommunicationPanel");
 
-        //creo div contenente la chiamata vera e propria
-        var divCall = document.createElement('div');
-        divCall.id = "divCall";
-        //creo div contenente le chat testuali
-        var divChat = document.createElement('div');
-        divChat.id = "divChat";
+		// azzero il div
+		element.innerHTML = "";
 
-        //creo gli elementi video per la videochat
-        var myVideo = document.createElement('video');
-        myVideo.id = "myVideo";
-        myVideo.setAttribute("autoplay", "autoplay");
-        var otherVideo = document.createElement('video');
-        otherVideo.id = "otherVideo";
-        otherVideo.setAttribute("autoplay", "autoplay");
-        
-        //creo i bottoni per per la gestione della chiamata
-        var closeButton = document.createElement('button');
-        closeButton.type = "button";
-        closeButton.id = "closeButton";
-        closeButton.onclick = communicationcenter.endCall;
+		// creo div contenente la chiamata vera e propria
+		var divCall = document.createElement('div');
+		divCall.id = "divCall";
+		// creo div contenente le chat testuali
+		var divChat = document.createElement('div');
+		divChat.id = "divChat";
 
-        //appendo i child al divCall
-        divCall.appendChild(myVideo);
-        divCall.appendChild(otherVideo);
-        divCall.appendChild(closeButton);
+		// creo gli elementi video per la videochat
+		var myVideo = document.createElement('video');
+		myVideo.id = "myVideo";
+		myVideo.setAttribute("autoplay", "autoplay");
+		var otherVideo = document.createElement('video');
+		otherVideo.id = "otherVideo";
+		otherVideo.setAttribute("autoplay", "autoplay");
 
-        //creo gli elementi per la chat testuale
-        //creo l'<ul> per le schede delle chat aperte
-        var ulOpenChat = document.createElement('ul');
-        ulOpenChat.id = "ulOpenChat";
-        
-        //crea gli HTMLDivElement con il testo della chat
-		for (var chat in communicationCenter.openChat) {
-			var element = document.createElement("div");
-			var form = document.createElement("form");
-			element.id = "divContainerChat";
-			var textArea = document.createElement("textarea");
-			element.appendChild(textArea);
-			
-			var input = document.createElement("input");
-			input.id = "text";
-			input.name = "text";
-			
+		// creo i bottoni per per la gestione della chiamata
+		var closeButton = document.createElement('button');
+		closeButton.type = "button";
+		closeButton.id = "closeButton";
+		closeButton.onclick = communicationcenter.endCall;
+
+		// appendo i child al divCall
+		divCall.appendChild(myVideo);
+		divCall.appendChild(otherVideo);
+		divCall.appendChild(closeButton);
+
+		// creo gli elementi per la chat testuale
+		// creo l'<ul> per le schede delle chat aperte
+		var ulOpenChat = document.createElement('ul');
+		ulOpenChat.id = "ulOpenChat";
+
+		// crea gli HTMLDivElement con il testo della chat
+		for ( var chat in communicationCenter.openChat) {
 			// appende l'elemento all'array associativo chatElements
-			chatElements[chat.id] = element;
+			chatElements[chat.id] = createChatElement(chat);
 		}
-		
-        //popola la lista delle chat
-        populateChatUl(ulOpenChat);
-        
-        //creo il div per la visualizzazione della chat selezionata.
-//        var divContainerChat = document.createElement('div');
-//        divContainerChat.id = "divContainerChat";
 
-        //appendo la lista  al <div> della chat
-        divChat.appendChild(ulOpenChat);
-//        divChat.appendChild(divContainerChat);
+		// popola la lista delle chat
+		populateChatUl(ulOpenChat);
 
-        //apendo il sottoalbero al DOM
-        element.appendChild(divCall);
-        element.appendChild(divChat);
+		// creo il div per la visualizzazione della chat selezionata.
+		// var divContainerChat = document.createElement('div');
+		// divContainerChat.id = "divContainerChat";
 
-        return element;
-    };
-    
-    
-    
+		// appendo la lista al <div> della chat
+		divChat.appendChild(ulOpenChat);
+		// divChat.appendChild(divContainerChat);
+
+		// apendo il sottoalbero al DOM
+		element.appendChild(divCall);
+		element.appendChild(divChat);
+
+		return element;
+	};
+
 }
