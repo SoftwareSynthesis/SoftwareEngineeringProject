2b588693182feba45b2022a968493296f75a36bf
Elena <elezecco@hotmail.it>
2013-03-22 11:31:23 +0100

    test setup (finalemente) finito!

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/AddressBookPanelPresenterTest.js
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/AddressBookPanelPresenterTest.js
@@ -25,11 +25,73 @@ test("testSetup()", function() {
 	var gr=document.createElement("select");
 	gr.id="selectGroup";
 	element.appendChild(gr);
-	
+	var i=0;
 	tester.setup();
+	var list=ulList.childNodes;
 	//controllo che abbia scaricato tutti contatti dal server
-	equal(ulList.childNodes.length, 2, "numero corretto di contatti nella rubrica");
-	expect(1);
+	equal(list.length, 2, "numero corretto di contatti nella rubrica");
+	i++;
+	
+	equal(list[0].nodeName, "LI", "il primo figlio dell'elemento e' un figlio della lista");
+	i++;
+	
+	var laura=list[1].childNodes;
+	
+	equal(laura.length,3, "ci sono tre figli");
+	i++;
+	
+	equal(laura[0].nodeName, "IMG", "il primo figlio dell'elemento e' un immagine(profilo)");
+	i++;
+	
+	equal(laura[1].nodeName, "#text", "il primo figlio dell'elemento e' un nodo testo");
+	i++;
+	
+	equal(laura[2].nodeName, "IMG", "il primo figlio dell'elemento e' un immagine(stato)");
+	i++;
+	
+	
+	equal(laura[0].getAttribute("src"), "y.png", "l'immagine (di Laura) è corretta");
+	i++;
+	
+	
+	equal(laura[1].data, "Laura Pausini", "il nome e congnome sono corretti");
+	i++;
+	
+	equal(laura[2].getAttribute("src"), "img/stateoffline.png", "lo stato (di Laura) e' corretto");
+	i++;
+	
+	
+	
+	var flavia=list[0].childNodes;
+	
+	equal(flavia.length,3, "ci sono tre figli");
+	i++;
+	
+	equal(flavia[0].nodeName, "IMG", "il primo figlio dell'elemento e' un immagine(profilo)");
+	i++;
+	
+	equal(flavia[1].nodeName, "#text", "il primo figlio dell'elemento e' un nodo testo");
+	i++;
+	
+	equal(flavia[2].nodeName, "IMG", "il primo figlio dell'elemento e' un immagine(stato)");
+	i++;
+	
+	
+	equal(flavia[0].getAttribute("src"), "x.png", "l'immagine (di Flavia) è corretta");
+	i++;
+	
+	
+	equal(flavia[1].data, "Flavia Bacco", "il nome e congnome sono corretti");
+	i++;
+	
+	equal(flavia[2].getAttribute("src"), "img/stateavailable.png", "lo stato (di Flavia) e' corretto");
+	i++;
+	
+	
+	
+	
+	
+	expect(i);
 });
 /*
 test("testInitialize()", function() {
