fc64d6775f0b91c6ec5076748e57afdfadf00381
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-25 21:00:18 +0100

    bugfix in contact-porca-puttana

--- Sources/ModelManager/WebContent/clientpresenter/guicontrol/ContactPanelPresenter.js
+++ Sources/ModelManager/WebContent/clientpresenter/guicontrol/ContactPanelPresenter.js
@@ -184,7 +184,7 @@ function ContactPanelPresenter() {
 				document.createTextNode(contact.surname));
 		document.getElementById("contactEmail").appendChild(
 				document.createTextNode(contact.email));
-		document.getElementById("contactAvatar").src = contact.image;
+		document.getElementById("contactAvatar").src = contact.picturePath;
 
 		// recupero i bottoni per associargli i metodi
 		var addToAddressBookButton = document
