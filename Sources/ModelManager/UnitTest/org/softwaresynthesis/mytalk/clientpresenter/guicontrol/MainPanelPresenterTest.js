8f6c9c2130d364b2fd5948ac8f88bc3f5bb178b0
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-26 15:59:26 +0100

    aggiunti coverage totali

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/MainPanelPresenterTest.js
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/MainPanelPresenterTest.js
@@ -1,6 +1,14 @@
 module("MainPanelPresenter", {
 	setup : function() {
+		// stub di interfaccia grafica
+		var element = document.createElement("div");
+		element.id = "MainPanel";
+		element.style.position = "absolute";
+		element.style.left = "-999em";
+		document.body.appendChild(element);
+		
 		tester = new MainPanelPresenter();
+		
 	},
 	teardown : function() {
 	}
