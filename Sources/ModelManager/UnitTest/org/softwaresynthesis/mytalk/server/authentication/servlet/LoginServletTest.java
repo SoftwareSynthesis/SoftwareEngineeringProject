6d267cf680acaa60a7933aec327bd6597f8be349
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-29 23:47:57 +0100

    aggiunti test per LogoutServlet, MOCKITO ROCKS

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/servlet/LoginServletTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/servlet/LoginServletTest.java
@@ -1,14 +1,16 @@
 package org.softwaresynthesis.mytalk.server.authentication.servlet;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import static org.mockito.Mockito.*;
+
+//
 
 public class LoginServletTest {
 
@@ -43,7 +45,7 @@ public class LoginServletTest {
 		
 		// verifica l'output della servlet
 		writer.flush();
-		String responseText = writer.getBuffer().toString();
+		String responseText = writer.toString();
 		assertFalse(responseText.length() == 0);
 		
 		//TODO approfondire questo test!
