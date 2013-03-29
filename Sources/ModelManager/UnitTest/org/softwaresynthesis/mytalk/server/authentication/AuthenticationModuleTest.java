03b7a9c22d578dde045a30d629117b7e5026f649
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-29 14:01:19 +0100

    fuffato authenticationmodule e aggiornata suite di test per dao

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/AuthenticationModuleTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/AuthenticationModuleTest.java
@@ -11,38 +11,35 @@ import javax.security.auth.login.LoginException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class AuthenticationModuleTest 
-{
+public class AuthenticationModuleTest {
 	private static LoginContext tester;
-	
+
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception 
-	{
-		System.setProperty("java.security.auth.login.config", System.getenv("MyTalkAuthentication"));
-		AuthenticationData data = new AuthenticationData("indirizzo5@dominio.it", "ciao");
+	public static void setUpBeforeClass() throws Exception {
+		System.setProperty("java.security.auth.login.config",
+				"/var/lib/tomcat7/webapps/ModelManager/Conf/LoginConfiguration.conf");
+		AuthenticationData data = new AuthenticationData(
+				"indirizzo5@dominio.it", "password");
 		CredentialLoader loader = new CredentialLoader(data, new AESAlgorithm());
-		tester = new LoginContext("LoginConf", loader);
+		tester = new LoginContext("Configuration", loader);
 	}
-	
+
 	@Test
-	public void testLogin()
-	{
+	public void testLogin() {
 		Subject subject = null;
 		Object[] principalsArray = null;
 		Set<Principal> principals = null;
 		String data = null;
-		try
-		{
+		try {
 			tester.login();
 			subject = tester.getSubject();
 			principals = subject.getPrincipals();
 			assertNotNull(principals);
 			principalsArray = principals.toArray();
 			data = principalsArray[0].toString();
-			assertEquals("indirizzo5@dominio.it", data);
-		}
-		catch (LoginException ex)
-		{
+			assertEquals("PrincipalImpl[element: indirizzo5@dominio.it]", data);
+		} catch (LoginException ex) {
+			System.err.println(ex.getMessage());
 			fail("Login fallito");
 		}
 	}
