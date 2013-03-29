426c36028394bdd7097c28b1069a2d7d22846772
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-29 23:25:32 +0100

    test sulla prima servlet con Mockito :))))))

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/servlet/LoginServletTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/servlet/LoginServletTest.java
@@ -1,54 +1,52 @@
 package org.softwaresynthesis.mytalk.server.authentication.servlet;
 
-import static org.junit.Assert.*;
-
-import java.io.File;
-
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletRequest;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.meterware.httpunit.PostMethodWebRequest;
-import com.meterware.httpunit.WebRequest;
-import com.meterware.httpunit.WebResponse;
-import com.meterware.servletunit.ServletRunner;
-import com.meterware.servletunit.ServletUnitClient;
+import static org.junit.Assert.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import static org.mockito.Mockito.*;
 
 public class LoginServletTest {
 
 	private static LoginServlet tester;
-	private static ServletRunner runner;
-	private static ServletUnitClient client;
-	
-	/*
-	 *  FIXME 
-	 *	String separator = System.getProperty("file.separator");
-	 *  path += separator + "Conf" + separator + "LoginConfiguration.conf";
-	 * 
-	 */
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	private StringWriter writer;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		File desc = new File("WebContent/WEB-INF/web.xml");
-		runner = new ServletRunner(desc);
-		runner.registerServlet("LoginAuthentication",
-				LoginServlet.class.getName());
-		client = runner.newClient();
+		tester = new LoginServlet();
+	}
+	
+	@Before
+	public void setUp() {
+		request = mock(HttpServletRequest.class);
+		response = mock(HttpServletResponse.class);
+		writer = new StringWriter();
 	}
 
 	@Test
-	public void testDoPostHttpServletRequestHttpServletResponse() {
-		try {
-			WebRequest request = new PostMethodWebRequest(
-					"http://LoginAuthentication");
-			request.setParameter("username", "indirizzo5@dominio.it");
-			request.setParameter("password", "password");
-			WebResponse response = client.getResponse(request);
-			assertNotNull(response);
-		} catch (Exception ex) {
-			fail(ex.getMessage());
-		}
+	public void testDoPost() throws Exception {
+		// configura il comportamento della richiesta
+		when(request.getParameter("username")).thenReturn(
+				"indirizzo5@dominio.it");
+		when(request.getParameter("password")).thenReturn("password");
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+		
+		// verifica l'output della servlet
+		writer.flush();
+		String responseText = writer.getBuffer().toString();
+		assertFalse(responseText.length() == 0);
+		
+		//TODO approfondire questo test!
 	}
 
 }
