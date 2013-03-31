8e19f5d5d29f2629c98f37cd7a65f8061e899e02
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-31 15:45:50 +0200

    eliminato errore di concetto in test_e_misurazioni, adeguato a norme e piano anche AddressBookDoSearchServletTest

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoSearchServletTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoSearchServletTest.java
@@ -14,41 +14,100 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+/**
+ * Verifica la possibilità di ottenere una lista di contatti tramite opportuni
+ * parametri (criteri) di ricerca sfruttando la servlet addetta alle ricerche,
+ * AddressBookDoSearchServlet, interrogata mediante il metodo POST
+ * 
+ * @author Diego Beraldin
+ */
 public class AddressBookDoSearchServletTest {
-	
+	// oggetto da testare
 	private static AddressBookDoSearchServlet tester;
+	// stub di richiesta HTTP
 	private HttpServletRequest request;
+	// stub di risposta HTTP
 	private HttpServletResponse response;
+	// writer su cui scrivere il testo della risposta
 	private StringWriter writer;
 
+	/**
+	 * Inizializza l'oggetto da verificare prima di tutti i test
+	 * 
+	 * @author Diego Beraldin
+	 */
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
 		tester = new AddressBookDoSearchServlet();
 	}
 
+	/**
+	 * Prima di ogni test, crea gli stub necessari per la sua esecuzione e
+	 * azzera il contenuto dello StringBuffer in cui dovrà essere memorizzato il
+	 * testo della risposta ottenuta dalla servlet
+	 * 
+	 * @author Diego Beraldin
+	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		request = mock(HttpServletRequest.class);
 		response = mock(HttpServletResponse.class);
 		writer = new StringWriter();
-		when(response.getWriter()).thenReturn(new PrintWriter(writer));
 	}
 
+	/**
+	 * Verifica che sia possibile effettuare una ricerca fra gli utenti
+	 * registrati al sistema in base a determinati criteri
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author Diego Beraldin
+	 */
 	@Test
-	public void testGetSearchContact() throws IOException, ServletException {		
+	public void testGetSearchContact() throws IOException, ServletException {
 		// configura il comportamento della richiesta
 		when(request.getParameter("param")).thenReturn("pippo");
-		
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
 		// invoca il metodo da testare
 		tester.doPost(request, response);
-		
+
 		// verifica l'output
 		writer.flush();
 		String responseText = writer.toString();
 		assertNotNull(responseText);
 		assertFalse(responseText.length() == 0);
 		// FIXME questo test non è completo e NON passa!
+	}
+
+	/**
+	 * Verifica la corretta gestione del caso in cui la richiesta HTTP non
+	 * contenga delle informazioni errate che impediscono di portare a termine
+	 * con successo l'operazione di ricerca
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author Diego Beraldin
+	 */
+	@Test
+	public void testWrongData() throws IOException, ServletException {
+		// configura il comportamento della richiesta (parametro errato)
+		when(request.getParameter("para")).thenReturn("pippo");
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+
+		// verifica l'output
+		writer.flush();
+		String responseText = writer.toString();
+		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
+		assertEquals("false", responseText);
 	}
 }
