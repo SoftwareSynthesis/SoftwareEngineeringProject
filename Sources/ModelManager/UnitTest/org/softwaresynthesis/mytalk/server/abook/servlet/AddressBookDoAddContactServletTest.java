e18cbc0aaabfad244e1fd9ca499a9a5f5566180a
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-31 12:28:46 +0200

    adeguato a norme+pianificazione AddressBookDoAddContactServletTest

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoAddContactServletTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoAddContactServletTest.java
@@ -16,44 +16,143 @@ import java.io.StringWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 
+/**
+ * Verifica la possibilità di aggiungere un contatto alla propria rubrica
+ * 
+ * @author diego
+ */
 public class AddressBookDoAddContactServletTest {
-	
+	// oggetto da testare
 	private static AddressBookDoAddContactServlet tester;
+	// stub di richiesta HTTP
 	private HttpServletRequest request;
+	// stub di risposta HTTP
 	private HttpServletResponse response;
+	// buffer che memorizza il testo della risposta
 	private StringWriter writer;
 
+	/**
+	 * Inizializza l'oggetto da testare prima di tutti i test
+	 * 
+	 * @author diego
+	 */
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
 		tester = new AddressBookDoAddContactServlet();
 	}
 
+	/**
+	 * Prima di ogni test, ricrea gli stub necessari alla sua esecuzione e
+	 * azzera la stringa in cui è rappresentato il testo della risposta
+	 * 
+	 * @author diego
+	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		request = mock(HttpServletRequest.class);
 		response = mock(HttpServletResponse.class);
 		writer = new StringWriter();
-		when(response.getWriter()).thenReturn(new PrintWriter(writer));
 	}
 
+	/**
+	 * Verifica la possibilità di aggiungere correttamente un contatto
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
 	@Test
 	public void testAddCorrectContact() throws IOException, ServletException {
 		// crea sessione identificativa per indirizzo5@dominio.it
 		HttpSession mySession = mock(HttpSession.class);
-		when(mySession.getAttribute("username")).thenReturn("indirizzo5@dominio.it");
-		
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
 		// configura il comportamento della richiesta
 		when(request.getSession(false)).thenReturn(mySession);
 		when(request.getParameter("contactId")).thenReturn("2");
 
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
 		// invoca il metodo da testare
 		tester.doPost(request, response);
-		
+
 		// verifica l'output
 		writer.flush();
 		String responseText = writer.toString();
 		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
 		assertEquals("true", responseText);
+		/*
+		 * FIXME occorre poi rimuovere dal database il contatto aggiunto se
+		 * questo è un test di integrazione come sembra essere!
+		 */
 	}
 
+	/**
+	 * Verifica l'impossibilità di aggiungere un contatto inesistente alla
+	 * rubrica (ID del contatto non valido)
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
+	@Test
+	public void testAddNotExistContact() throws IOException, ServletException {
+		// crea sessione identificativa per indirizzo5@dominio.it
+		HttpSession mySession = mock(HttpSession.class);
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
+		// configura il comportamento della richiesta
+		when(request.getSession(false)).thenReturn(mySession);
+		when(request.getParameter("contactId")).thenReturn("-1");
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+
+		// verifica l'output
+		writer.flush();
+		String responseText = writer.toString();
+		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
+		assertEquals("false", responseText);
+	}
+
+	/**
+	 * Verifica l'impossibilità di portare a termine l'operazione nel caso in
+	 * cui non siano forniti tutti i dati necessari all'aggiunta di un nuovo
+	 * contatto alla rubrica
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
+	@Test
+	public void testAddWrongData() throws IOException, ServletException {
+		// crea sessione identificativa per indirizzo5@dominio.it
+		HttpSession mySession = mock(HttpSession.class);
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
+		// configura il comportamento della richiesta (manca un parametro)
+		when(request.getSession(false)).thenReturn(mySession);
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+
+		// verifica l'output
+		writer.flush();
+		String responseText = writer.toString();
+		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
+		assertEquals("false", responseText);
+	}
 }
