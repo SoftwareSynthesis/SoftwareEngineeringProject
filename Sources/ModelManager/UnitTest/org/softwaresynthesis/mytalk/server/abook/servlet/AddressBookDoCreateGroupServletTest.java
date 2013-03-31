b60f048187a883bd509049537037e74cbd364df5
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-31 12:54:47 +0200

    adeguato a norme+piano AddressBookDoCreateGroupServlet

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoCreateGroupServletTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoCreateGroupServletTest.java
@@ -16,40 +16,107 @@ import java.io.StringWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 
+/**
+ * Verifica la possibilità di creare un nuovo gruppo nella rubrica di un utente
+ * 
+ * @author diego
+ */
 public class AddressBookDoCreateGroupServletTest {
+	// oggetto da testare
 	private static AddressBookDoCreateGroupServlet tester;
+	// stub di richiesta HTTP
 	private HttpServletRequest request;
+	// stub di risposta HTTP
 	private HttpServletResponse response;
+	// contiene il buffer su cui sarà memorizzato il testo della risposta
 	private StringWriter writer;
 
+	/**
+	 * Inizializza l'oggetto da testare prima di tutti i test
+	 * 
+	 * @author diego
+	 */
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
 		tester = new AddressBookDoCreateGroupServlet();
 	}
 
+	/**
+	 * Prima di ogni test, ricrea gli stub necessari alla sia esecuzione e
+	 * azzera il contenuto del buffer in cui sarà salvata la risposta
+	 * 
+	 * @author diego
+	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		request = mock(HttpServletRequest.class);
 		response = mock(HttpServletResponse.class);
 		writer = new StringWriter();
-		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
 	}
 
+	/**
+	 * Verifica la possibilità di aggiungere con successo un gruppo alla propria
+	 * rubrica
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
 	@Test
 	public void testAddCorrectGroup() throws IOException, ServletException {
 		// crea sessione di autenticazione
 		HttpSession mySession = mock(HttpSession.class);
-		when(mySession.getAttribute("username")).thenReturn("indirizzo5@dominio.it");
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
 		// configura il comportamento della richiesta
 		when(request.getSession(false)).thenReturn(mySession);
 		when(request.getParameter("groupName")).thenReturn("gruppo0");
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
 		// invoca il metodo da testare
 		tester.doPost(request, response);
+
 		// verifica l'output
 		writer.flush();
 		String responseText = writer.toString();
 		assertNotNull(responseText);
 		assertFalse(responseText.length() == 0);
 		assertEquals("true", responseText);
+	}
+
+	/**
+	 * Verifica l'impossibilità di aggiungere un gruppo alla propria rubrica
+	 * qualora non siano forniti tutti i dati obbligatori per l'operazione
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
+	@Test
+	public void testAddWrongData() throws IOException, ServletException {
+		// crea sessione di autenticazione
+		HttpSession mySession = mock(HttpSession.class);
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
+		// configura il comportamento della richiesta (manca parametro)
+		when(request.getSession(false)).thenReturn(mySession);
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+
+		// verifica l'output
+		writer.flush();
+		String responseText = writer.toString();
+		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
+		assertEquals("false", responseText);
 	}
 }
