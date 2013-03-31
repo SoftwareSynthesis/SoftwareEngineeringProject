264a4439fcbb79f64717de1ae412244419e49d2a
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-31 15:20:07 +0200

    uniformati alle norme AddressBookDoBlockServletTest e AddressBookDoUnblockServletTest

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoBlockServletTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoBlockServletTest.java
@@ -16,39 +16,71 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * Verifica la possibilità di bloccare un contatto all'interno di una rubrica
+ * 
+ * @author diego
+ */
 public class AddressBookDoBlockServletTest {
-	
+	// oggetto da testare
 	private static AddressBookDoBlockServlet tester;
+	// stub di richiesta HTTP
 	private HttpServletRequest request;
+	// stub di riposta HTTP
 	private HttpServletResponse response;
+	// contiene lo StringBuffer in cui sarà memorizzato il testo della risposta
 	private StringWriter writer;
 
+	/**
+	 * Inizializza l'oggetto da verificare prima di tutti i test
+	 * 
+	 * @author diego
+	 */
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
 		tester = new AddressBookDoBlockServlet();
 	}
 
+	/**
+	 * Ricrea prima di ogni test gli stub necessari alla sua esecuzione e azzera
+	 * il buffer in cui verrà memorizzato il testo della risposta
+	 * 
+	 * @author diego
+	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		request = mock(HttpServletRequest.class);
 		response = mock(HttpServletResponse.class);
 		writer = new StringWriter();
-		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
 	}
 
+	/**
+	 * Verifica l'avvenuto blocco del contatto passato come parametro, che
+	 * appartiene effettivamente alla rubrica dell'utente da cui proviene la
+	 * richiesta
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
 	@Test
 	public void testBlockCorrectContact() throws IOException, ServletException {
 		// crea una sessione di autenticazione
 		HttpSession mySession = mock(HttpSession.class);
-		when(mySession.getAttribute("username")).thenReturn("indirizzo5@dominio.it");
-		
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
 		// configura il comportamento della richiesta
 		when(request.getSession(false)).thenReturn(mySession);
 		when(request.getParameter("contactId")).thenReturn("3");
-		
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
 		// invoca il metodo da testare
 		tester.doPost(request, response);
-		
+
 		// verifica l'output
 		writer.flush();
 		String responseText = writer.toString();
@@ -57,20 +89,60 @@ public class AddressBookDoBlockServletTest {
 		assertFalse(responseText.length() == 0);
 		assertEquals("true", responseText);
 	}
-	
+
+	/**
+	 * Verifica l'impossibilità di bloccare un contatto che non appartiene alla
+	 * rubrica dell'utente da cui proviene la richiesta
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
 	@Test
 	public void testBlockNotExistContact() throws IOException, ServletException {
 		// crea una sessione di autenticazione
 		HttpSession mySession = mock(HttpSession.class);
-		when(mySession.getAttribute("username")).thenReturn("indirizzo5@dominio.it");
-		
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
 		// configura il comportamento della richiesta
 		when(request.getSession(false)).thenReturn(mySession);
 		when(request.getParameter("contactId")).thenReturn("-1");
-		
+
 		// invoca il metodo da testare
 		tester.doPost(request, response);
-		
+
+		// verifica l'output
+		writer.flush();
+		String responseText = writer.toString();
+		System.err.println(responseText);
+		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
+		assertEquals("false", responseText);
+	}
+
+	/**
+	 * Verifica l'impossibilità di bloccare un contatto nel momento in cui la
+	 * richiesta che giunge alla servlet non contiene tutti i parametri
+	 * necessari per portare a termine l'operazione
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
+	@Test
+	public void testWrongData() throws IOException, ServletException {
+		// crea una sessione di autenticazione
+		HttpSession mySession = mock(HttpSession.class);
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
+		// configura il comportamento della richiesta (manca il parametro)
+		when(request.getSession(false)).thenReturn(mySession);
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+
 		// verifica l'output
 		writer.flush();
 		String responseText = writer.toString();
--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoUnblockServletTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoUnblockServletTest.java
@@ -16,39 +16,71 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * Verifica la possibilità di sboccare un contatto all'interno della rubrica
+ * 
+ * @author Diego Beraldin
+ */
 public class AddressBookDoUnblockServletTest {
-	
+	// oggetto da testare
 	private static AddressBookDoUnblockServlet tester;
+	// stub di richiesta HTTP
 	private HttpServletRequest request;
+	// stub di risposta HTTP
 	private HttpServletResponse response;
+	// contiene il buffer su cui è scritto il testo della risposta
 	private StringWriter writer;
 
+	/**
+	 * Inizializza l'oggetto da verificare prima di tutti i test
+	 * 
+	 * @author Diego Beraldin
+	 */
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
 		tester = new AddressBookDoUnblockServlet();
 	}
 
+	/**
+	 * Prima dell'esecuzione di ogni test, crea gli stub necessari alla sua
+	 * esecuzione e azzera il buffer in cui verrà memorizzato il testo della
+	 * risposta proveniente dalla servlet
+	 * 
+	 * @author Diego Beraldin
+	 */
 	@Before
 	public void setUp() throws Exception {
 		request = mock(HttpServletRequest.class);
 		response = mock(HttpServletResponse.class);
 		writer = new StringWriter();
-		when(response.getWriter()).thenReturn(new PrintWriter(writer));
 	}
 
+	/**
+	 * Verifica la possibilità di bloccare un contatto presente all'interno
+	 * della rubrica dell'utente da cui proviene la richiesta
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author Diego Beraldin
+	 */
 	@Test
-	public void testUnblockCorrectContact() throws IOException, ServletException {
+	public void testUnblockCorrectContact() throws IOException,
+			ServletException {
 		// crea una sessione di autenticazione
 		HttpSession mySession = mock(HttpSession.class);
-		when(mySession.getAttribute("username")).thenReturn("indirizzo5@dominio.it");
-		
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
 		// configura il comportamento della richiesta
 		when(request.getSession(false)).thenReturn(mySession);
 		when(request.getParameter("contactId")).thenReturn("3");
-		
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
 		// invoca il metodo da testare
 		tester.doPost(request, response);
-		
+
 		// verifica l'output
 		writer.flush();
 		String responseText = writer.toString();
@@ -57,20 +89,61 @@ public class AddressBookDoUnblockServletTest {
 		assertFalse(responseText.length() == 0);
 		assertEquals("true", responseText);
 	}
-	
+
+	/**
+	 * Verifica la corretta gestione di una richiesta di sblocco per un contatto
+	 * che non corrisponde ad alcun utente registrato nel sistema
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author Diego Beraldin
+	 */
 	@Test
-	public void testUnblockNotExistContact() throws IOException, ServletException {
+	public void testUnblockNotExistContact() throws IOException,
+			ServletException {
 		// crea una sessione di autenticazione
 		HttpSession mySession = mock(HttpSession.class);
-		when(mySession.getAttribute("username")).thenReturn("indirizzo5@dominio.it");
-		
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
 		// configura il comportamento della richiesta
 		when(request.getSession(false)).thenReturn(mySession);
 		when(request.getParameter("contactId")).thenReturn("-1");
-		
+
 		// invoca il metodo da testare
 		tester.doPost(request, response);
-		
+
+		// verifica l'output
+		writer.flush();
+		String responseText = writer.toString();
+		System.err.println(responseText);
+		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
+		assertEquals("false", responseText);
+	}
+
+	/**
+	 * Verifica la corretta gestione di una richiesta di sbloccare un contatto
+	 * nel momento in cui non vengano forniti tutti i parametri necessari per
+	 * portare a termine l'operazione
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author Diego Beraldin
+	 */
+	@Test
+	public void testWrongData() throws IOException, ServletException {
+		// crea una sessione di autenticazione
+		HttpSession mySession = mock(HttpSession.class);
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
+		// configura il comportamento della richiesta (manca il parametro)
+		when(request.getSession(false)).thenReturn(mySession);
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+
 		// verifica l'output
 		writer.flush();
 		String responseText = writer.toString();
