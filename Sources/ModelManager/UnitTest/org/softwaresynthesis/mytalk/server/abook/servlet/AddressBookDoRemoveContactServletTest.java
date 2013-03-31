a1e046392bc648717fed32eca90df021b33df291
Beraldin Diego <diego.beraldin@gmail.com>
2013-03-31 12:45:58 +0200

    adeguato alle norme e al piano AddresBookDoRemoveContactServletTest

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoRemoveContactServletTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/servlet/AddressBookDoRemoveContactServletTest.java
@@ -3,6 +3,7 @@ package org.softwaresynthesis.mytalk.server.abook.servlet;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import org.junit.Before;
@@ -16,39 +17,69 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * Verifica la possibilità di rimuovere un utente dalla propria rubrica
+ * 
+ * @author diego
+ */
 public class AddressBookDoRemoveContactServletTest {
-	
+	// oggetto da testare
 	private static AddressBookDoRemoveContactServlet tester;
+	// stub di richiesta HTTP
 	private HttpServletRequest request;
+	// stub di risposta HTTP
 	private HttpServletResponse response;
+	// oggetto per simulare il writer della risposta
 	private StringWriter writer;
 
+	/**
+	 * Inizializza l'oggetto da testare prima di tutti i test
+	 * 
+	 * @author diego
+	 */
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
 		tester = new AddressBookDoRemoveContactServlet();
 	}
 
+	/**
+	 * Prima di ogni test, crea gli stub necessari alla sua esecuzione e azzera
+	 * il buffer in cui verrà memorizzato il testo della risposta
+	 * 
+	 * @author diego
+	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		request = mock(HttpServletRequest.class);
 		response = mock(HttpServletResponse.class);
 		writer = new StringWriter();
-		when(response.getWriter()).thenReturn(new PrintWriter(writer));
 	}
 
+	/**
+	 * Verifica la possibilità di rimuovere un utente dalla rubrica identificato
+	 * da dati corretti e coerenti con il contenuto del database
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
 	@Test
 	public void testRemoveCorrectUser() throws IOException, ServletException {
 		// crea una sessione di autenticazione
 		HttpSession mySession = mock(HttpSession.class);
-		when(mySession.getAttribute("username")).thenReturn("indirizzo5@dominio.it");
-		
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
 		// configura il comportamento della richiesta
 		when(request.getSession(false)).thenReturn(mySession);
 		when(request.getParameter("contactId")).thenReturn("3");
-		
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
 		// invoca il metodo da testare
 		tester.doPost(request, response);
-		
+
 		// verifica l'output
 		writer.flush();
 		String responseText = writer.toString();
@@ -56,6 +87,78 @@ public class AddressBookDoRemoveContactServletTest {
 		assertNotNull(responseText);
 		assertFalse(responseText.length() == 0);
 		assertEquals("true", responseText);
+	}
+
+	/**
+	 * Verifica l'impossibilità di rimuovere dalla rubrica un contatto che non
+	 * esiste nel sistema (ID non valido)
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
+	@Test
+	public void testRemoveNotExistContact() throws IOException,
+			ServletException {
+		// crea una sessione di autenticazione
+		HttpSession mySession = mock(HttpSession.class);
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
+		// configura il comportamento della richiesta
+		when(request.getSession(false)).thenReturn(mySession);
+		when(request.getParameter("contactId")).thenReturn("-1");
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+
+		// verifica l'output
+		writer.flush();
+		String responseText = writer.toString();
+		System.err.println(responseText);
+		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
+		assertEquals("false", responseText);
+	}
+	
+	public void testRemoveNotFriendContact() throws IOException, ServletException {
+		fail("Non ancora implementato");
+	}
+
+	/**
+	 * Verifica l'impossibilità di rimuovere dalla rubrica un contatto se le
+	 * informazioni passate alla servlet non sono complete
+	 * 
+	 * @throws IOException
+	 * @throws ServletException
+	 * @author diego
+	 */
+	@Test
+	public void testRemoveWrongData() throws IOException, ServletException {
+		// crea una sessione di autenticazione
+		HttpSession mySession = mock(HttpSession.class);
+		when(mySession.getAttribute("username")).thenReturn(
+				"indirizzo5@dominio.it");
+
+		// configura il comportamento della richiesta (manca un parametro)
+		when(request.getSession(false)).thenReturn(mySession);
+
+		// configura il comportamento della risposta
+		when(response.getWriter()).thenReturn(new PrintWriter(writer));
+
+		// invoca il metodo da testare
+		tester.doPost(request, response);
+
+		// verifica l'output
+		writer.flush();
+		String responseText = writer.toString();
+		System.err.println(responseText);
+		assertNotNull(responseText);
+		assertFalse(responseText.length() == 0);
+		assertEquals("false", responseText);
 	}
 
 }
