95445ab04794e32e7ff0d5b397869279060889d8
Beraldin Diego <diego.beraldin@gmail.com>
2013-04-01 17:31:56 +0200

    rivisti (ma sono da completare!) test su AuthenticationModule

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/AuthenticationModuleTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/AuthenticationModuleTest.java
@@ -1,23 +1,30 @@
 package org.softwaresynthesis.mytalk.server.authentication;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import java.security.Principal;
 import java.util.Set;
-
 import javax.security.auth.Subject;
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * Verifica la possibilità di effettuare un login al sistema
+ *
+ * @author Andrea Meneghinello
+ * @author Diego Beraldin
+ */
 public class AuthenticationModuleTest {
+	// FIXME tutto questo test è da rivedere!
 	private static LoginContext tester;
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws LoginException {
 		System.setProperty("java.security.auth.login.config",
-				"/var/lib/tomcat7/webapps/ModelManager/Conf/LoginConfiguration.conf");
+				"/var/lib/tomcat7/webapps/MyTalk/Conf/LoginConfiguration.conf");
 		AuthenticationData data = new AuthenticationData(
 				"indirizzo5@dominio.it", "password");
 		CredentialLoader loader = new CredentialLoader(data, new AESAlgorithm());
@@ -39,8 +46,7 @@ public class AuthenticationModuleTest {
 			data = principalsArray[0].toString();
 			assertEquals("PrincipalImpl[element: indirizzo5@dominio.it]", data);
 		} catch (LoginException ex) {
-			System.err.println(ex.getMessage());
-			fail("Login fallito");
+			fail(ex.getMessage());
 		}
 	}
 }
