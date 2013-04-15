4b5654b5de9c7f544e5addd906fb02ee6d675386
Diego Beraldin <diego.beraldin@gmail.com>
2013-04-15 09:38:36 +0200

    eliminato bug e riorganizzato SearchResultPP

--- Sources/ModelManager/WebContent/clientpresenter/guicontrol/SearchResultPanelPresenter.js
+++ Sources/ModelManager/WebContent/clientpresenter/guicontrol/SearchResultPanelPresenter.js
@@ -1,80 +1,102 @@
 /**
- * Presenter incaricato di gestire il pannello che visualizza i risultati di ricerca
+ * Presenter incaricato di gestire il pannello che visualizza i risultati di
+ * ricerca
  * 
  * @constructor
  * @this {SearchResultPanelPresenter}
- * @param {String} url URL della servlet con cui il presenter deve parlare
+ * @param {String}
+ *            url URL della servlet con cui il presenter deve parlare
  * @author Marco Schivo
  * @author Diego Beraldin
  */
 function SearchResultPanelPresenter(url) {
-/**********************************************************
-                      VARIABILI PRIVATE
-***********************************************************/
+	/***************************************************************************
+	 * VARIABILI PRIVATE
+	 **************************************************************************/
 	// indirizzo della servlet con cui il presenter deve interfacciarsi
 	var servlets = new Array();
 	// inizializza i normi delle servlet
 	getServletURLs();
 
-/**********************************************************
-                       METODI PRIVATI
-***********************************************************/
+	/***************************************************************************
+	 * METODI PRIVATI
+	 **************************************************************************/
 	function getServletURLs() {
-		 var configurationRequest = new XMLHttpRequest();
-	     configurationRequest.open("POST", configurationFile, false);
-	     configurationRequest.send();
-	     var XMLDocument = configurationRequest.responseXML;
-	     var baseURL = XMLDocument.getElementsByTagName("baseURL")[0].childNodes[0].data;
-	     var name = XMLDocument.getElementById("search").childNodes[0].data;
-	     servlets.push(baseURL + name);
+		var configurationRequest = new XMLHttpRequest();
+		configurationRequest.open("POST", configurationFile, false);
+		configurationRequest.send();
+		var XMLDocument = configurationRequest.responseXML;
+		var baseURL = XMLDocument.getElementsByTagName("baseURL")[0].childNodes[0].data;
+		var name = XMLDocument.getElementById("search").childNodes[0].data;
+		servlets.push(baseURL + name);
 	}
-	
+
 	/**
-	 * Aggiunge un contatto ad una lista (pensato per essere visualizzato
-	 * nel pannello dei risultati di una ricerca, in cui è quindi possibile
-	 * anche aggiungere un nuovo contatto alla propria rubrica
+	 * Crea l'etichett che visualizza i dati dell'utente (se sono presenti) e in
+	 * ogni caso mostra l'email memorizzata nel sistema. La funzione è
+	 * utilizzata per generare la lista dei contatti
 	 * 
-	 * @param {HTMLUListElement} list
-	 * @param {Object} contact
+	 * @param contact
+	 *            il contatto a partire dal quale deve essere generata
+	 *            l'etichetta del nome
+	 * @returns {String} l'etichetta di nome
 	 * @author Marco Schivo
+	 */
+	function createNameLabel(contact) {
+		var name = "";
+		if (contact.name != null)
+			name += contact.name;
+		if (contact.surname != null) {
+			if (name != "")
+				name += " ";
+			name += contact.surname;
+		}
+		if (name != "")
+			name += " - ";
+		// visualizza sempre l'email
+		name += contact.mail;
+		return name;
+	}
+
+	/**
+	 * Aggiunge un contatto ad una lista (pensato per essere visualizzato nel
+	 * pannello dei risultati di una ricerca, in cui è quindi possibile anche
+	 * aggiungere un nuovo contatto alla propria rubrica
+	 * 
+	 * @param {HTMLUListElement}
+	 *            list la lista cui deve essere aggiunta la nuova voce
+	 * @param {Object}
+	 *            contact il contatto a partire dal quale generare la voce da
+	 *            aggiungere alla listas
+	 * @see PresenterMediator#onContactSeleted(contact)
 	 * @author Diego Beraldin
 	 */
 	function addListItem(list, contact) {
 		var item = document.createElement("li");
-        item.setAttribute("id", contact.id);
+		item.setAttribute("id", contact.id);
 		// costruisce il nodo
-        var name = "";
-        if (contact.name != null)
-            name += contact.name;
-        if (contact.surname != null) {
-            if (name != "")
-                name += " ";
-            name += contact.surname;
-        }
-        if (name != "")
-        	name += " ";
-        // visualizza sempre l'email
-        name += contact.mail;
-        var avatarNode = document.createElement('img');
-        avatarNode.setAttribute("src", contact.picturePathatar);
-        var textNode = document.createTextNode(name);
-        // aggiunge i sottonodi al 'li' appena creato
-        item.appendChild(avatarNode);
-        item.appendChild(textNode);
-        item.appendChild(statusNode);
-        // comportamento del list item al click del mouse
-        item.onclick = function() {
-        	mediator.onContactSelected(contact);
-        };
-        item.appendChild(button);
-        
+		var avatarNode = document.createElement('img');
+		avatarNode.setAttribute("src", contact.picturePathatar);
+		var name = createNameLabel(contact);
+		var textNode = document.createTextNode(name);
+		// aggiunge i sottonodi al 'li' appena creato
+		item.appendChild(avatarNode);
+		item.appendChild(textNode);
+		// FIXME cos'è statusNode??!?!?
+		item.appendChild(statusNode);
+		// comportamento del list item al click del mouse
+		item.onclick = function() {
+			mediator.onContactSelected(contact);
+		};
+		item.appendChild(button);
+
 		// aggiunge il 'li' alla lista ricevuta come parametro
 		list.appendChild(item);
 	}
-	
-/**********************************************************
-                      METODI PUBBLICI
-***********************************************************/
+
+	/***************************************************************************
+	 * METODI PUBBLICI
+	 **************************************************************************/
 	/**
 	 * Inizializza il pannello dei risultati ricerca
 	 * 
@@ -84,21 +106,24 @@ function SearchResultPanelPresenter(url) {
 		var element = document.createElement("div");
 		element.setAttribute("id", "SearchResultPanel");
 		userList = document.createElement("ul");
+		userList.id = "userList"
 		// aggiunge al sottoalbero il nuovo elemento
 		element.appendChild(userList);
 		return element;
 	};
-	
+
 	/**
-	 * Visualizza all'interno del pannello una lista di contatti che
-	 * è stata ottenuta dal server a seguito di una ricerca
+	 * Visualizza all'interno del pannello una lista di contatti che è stata
+	 * ottenuta dal server a seguito di una ricerca
 	 * 
-	 * @param {Array} contacts la lista di contatti che deve essere visualizzata
+	 * @param {Array}
+	 *            contacts la lista di contatti che deve essere visualizzata
 	 * @author Diego Beraldin
 	 */
 	this.displayContactList = function(contacts) {
+		var userList = document.getElementById("userList");
 		userList.innerHTML = "";
-		for (var c in contacts) {
+		for ( var c in contacts) {
 			addListItem(userList, c);
 		}
 	};
