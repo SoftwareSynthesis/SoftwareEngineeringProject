0a5bb6e6f1d3f3e73e74dd05bcf3d66cb25a3f1b
Marco Schivo <marcoskivo@gmail.com>
2013-04-17 16:28:53 +0200

    inserito tracciamento servlet getMessages

--- Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/message/servlet/GetMessagesServlet.java
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/message/servlet/GetMessagesServlet.java
@@ -1,18 +1,14 @@
 package org.softwaresynthesis.mytalk.server.message.servlet;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.softwaresynthesis.mytalk.server.abook.IAddressBookEntry;
 import org.softwaresynthesis.mytalk.server.dao.MessageDAO;
 import org.softwaresynthesis.mytalk.server.message.IMessage;
 
