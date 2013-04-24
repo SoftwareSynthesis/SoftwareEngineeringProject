a1aaca1014abe293302beaadc676c4296f82bcf7
Riccardo Tresoldi <riccardo.tresoldi@me.com>
2013-04-24 15:40:40 +0200

    ADD funzioni generiche engine

--- Sources/ModelManager/WebContent/Initialize.js
+++ Sources/ModelManager/WebContent/Initialize.js
@@ -1,4 +1,8 @@
 window.onload = function() {
+    //Inizializzazione delle funzioni
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+    window.URL = window.URL || window.webkitURL;
     // VARIABILI GLOBALI per cui i programmatori meritano il taglio delle dita
     configurationFile = "Conf/servletlocation.xml";
     mediator = new PresenterMediator();
@@ -16,38 +20,37 @@ Object.size = function(obj) {
     return size;
 };
 
+/* TODO da sistemare perchè non va bene
 // alternativa alla versione successiva che non capisco tanto bene (diego)
-
 var isAboutToExit = false;
 
 window.onbeforeunload = function() {
-	var answer = confirm("Sicuro di voler uscire?");
-	if (answer) {
-		isAboutToExit = true;
-	}
+    var answer = confirm("Sicuro di voler uscire?");
+    if (answer) {
+        isAboutToExit = true;
+    }
 };
 window.onunload = function() {
-	if (isAboutToExit) {
-		mediator.logout();
-	}
+    if (isAboutToExit) {
+        mediator.logout();
+    }
 };
-
 
 //fatto da Stefano, eventualmente da sistemare (forse...)
 var needToConfirm = true;
 window.onbeforeunload = askConfirm;
 window.onunload = unloadPage;
 var isDelete = true;
-function unloadPage(){
-	if(isDelete){
-		logout();
-	}   
+function unloadPage() {
+    if (isDelete) {
+        logout();
+    }
 }
-function askConfirm(){
-	if (needToConfirm){
-		return "Verra' effettuato automaticamente il logout.";
-	} 
-	else{
-		isDelete = false;
-	}   
-}
+
+function askConfirm() {
+    if (needToConfirm) {
+        return "Verra' effettuato automaticamente il logout.";
+    } else {
+        isDelete = false;
+    }
+}*/
