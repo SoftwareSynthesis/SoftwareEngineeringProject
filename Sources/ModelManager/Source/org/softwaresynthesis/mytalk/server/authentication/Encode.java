b89f1ebd7d1d0063827b567f6c802268a3117b6b
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-04-28 17:10:05 +0200

    Algoritmo di crittografia
    Definito l'algoritmo per la crittografia della password e della risposta
    alla domanda segreta dell'utente. Come algoritmo si implementato AES a
    128 bit

---
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/authentication/Encode.java
@@ -0,0 +1,28 @@
+package org.softwaresynthesis.mytalk.server.authentication;
+
+import javax.crypto.Cipher;
+import sun.misc.BASE64Encoder;
+
+/**
+ * Rappresenta il passo finale dell'algoritmo
+ * di crittografia AES a 128 bit. Questo passo
+ * codifica utilizzando AES a 128 bit
+ * 
+ * @author 	Andrea Meneghinello
+ * @version 3.0
+ */
+public class Encode extends CryptoTemplate 
+{	
+	@Override
+	public String completeAlgorithm(String text) throws Exception 
+	{
+		BASE64Encoder encoder = new BASE64Encoder();
+		byte[] buffer = text.getBytes();
+		Cipher cipher = super.getCipher();
+		String result = null;
+		cipher.init(Cipher.ENCRYPT_MODE, super.getGeneratedKey());
+		buffer = cipher.doFinal(buffer);
+		result = encoder.encode(buffer);
+		return result;
+	}
+}
