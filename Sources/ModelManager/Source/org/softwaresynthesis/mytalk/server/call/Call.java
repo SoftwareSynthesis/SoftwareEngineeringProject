46c5609c2b1014ed3a2080265d83c76d2b977ff0
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-04-28 11:46:59 +0200

    Definzione di transfer object
    Definizione dei transfer object per la gestione dello storico delle
    chiamate effettuate tramite il sistema MyTalk

---
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/call/Call.java
@@ -0,0 +1,126 @@
+package org.softwaresynthesis.mytalk.server.call;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Rappresenta una chiamata effettuata con
+ * il sistema MyTalk
+ * 
+ * @author 	Andrea Meneghinello
+ * @version	3.0
+ */
+public class Call implements ICall 
+{
+	private Long id;
+	private Date start;
+	private Date end;
+	private Set<ICallList> calls;
+	
+	/**
+	 * Costruisce una nuova istanza priva
+	 * di valori
+	 */
+	public Call()
+	{
+		this(-1L);
+	}
+	
+	/**
+	 * Costruisce una nuova istanza assegnandogli
+	 * un identificativo
+	 * 
+	 * @param 	identifier	{@link Long} identificativo
+	 * 						associato
+	 */
+	public Call(Long identifier)
+	{
+		this.id = identifier;
+		this.calls = new HashSet<ICallList>();
+	}
+	
+	@Override
+	public Long getId() 
+	{
+		return this.id;
+	}
+
+	@Override
+	public Date getStartDate() 
+	{
+		return this.start;
+	}
+
+	@Override
+	public void setStartDate(Date startDate)
+	{
+		this.start = startDate;
+	}
+
+	@Override
+	public Date getEndDate() 
+	{
+		return this.end;
+	}
+
+	@Override
+	public void setDate(Date endDate) 
+	{
+		this.end = endDate;
+	}
+
+	@Override
+	public Set<ICallList> getCallList() 
+	{
+		return this.calls;
+	}
+
+	@Override
+	public void setCallList(Set<ICallList> callList) 
+	{
+		this.calls = callList;
+	}
+
+	@Override
+	public boolean addCall(ICallList call) 
+	{
+		boolean result = false;
+		call.setCall(this);
+		result = this.calls.add(call);
+		return result;
+	}
+
+	@Override
+	public boolean removeCall(ICallList call) 
+	{
+		boolean result = this.calls.remove(call);
+		return result;
+	}
+	
+	/**
+	 * Determina se due istanze rappresentano lo stesso
+	 * oggetto Call
+	 * 
+	 * @param	obj	{@link Object} istanza da verifcare
+	 * @return	true se le due istanze rappresentano lo
+	 * 			stesso oggetto, false altrimenti
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		boolean result = false;
+		Call toCompare = null;
+		Long toCompareId = null;
+		if (obj instanceof Call)
+		{
+			toCompare = (Call)obj;
+			toCompareId = toCompare.getId();
+			if (this.id.equals(toCompareId))
+			{
+				result = true;
+			}
+		}
+		return result;
+	}
+}
---
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/call/CallList.java
@@ -0,0 +1,110 @@
+package org.softwaresynthesis.mytalk.server.call;
+
+import org.softwaresynthesis.mytalk.server.abook.IUserData;
+
+/**
+ * Rappresenta informazioni di dettaglio per
+ * una chiamata effettuata con il sistema
+ * MyTalk
+ * 
+ * @author 	Andrea Meneghinello
+ * @version 3.0
+ */
+public class CallList implements ICallList
+{
+	private Long id;
+	private IUserData user;
+	private ICall call;
+	private boolean caller;
+	
+	/**
+	 * Costruisce una nuova istanza priva
+	 * di valori
+	 */
+	public CallList()
+	{
+		this(-1L);
+	}
+	
+	/**
+	 * Costruisce una nuova istanza assegnandogli
+	 * un identificativo
+	 * 
+	 * @param 	identifier	{@link Long} identificativo
+	 * 						associato
+	 */
+	public CallList(Long identifier)
+	{
+		this.id = identifier;
+	}
+	
+	@Override
+	public Long getId()
+	{
+		return this.id;
+	}
+
+	@Override
+	public IUserData getUser() 
+	{
+		return this.user;
+	}
+
+	@Override
+	public void setUser(IUserData user) 
+	{
+		this.user = user;
+	}
+
+	@Override
+	public ICall getCall()
+	{
+		return this.call;
+	}
+
+	@Override
+	public void setCall(ICall call) 
+	{
+		this.call = call;
+	}
+
+	@Override
+	public boolean getCaller() 
+	{
+		return this.caller;
+	}
+
+	@Override
+	public void setCaller(boolean caller) 
+	{
+		this.caller = caller;
+	}
+	
+	/**
+	 * Determina se due istanze rappresenta lo stesso
+	 * oggetto CallList
+	 * 
+	 * @param	obj	{@link Object} istanza da verificare
+	 * @return 	true se le due istanze rappresentano lo
+	 * 			stesso oggetto, false altrimenti
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		boolean result = false;
+		ICall toCompareCall = null;
+		CallList toCompare = null;
+		IUserData toCompareUser = null;
+		if (obj instanceof CallList)
+		{
+			toCompare = (CallList)obj;
+			toCompareCall = toCompare.getCall();
+			toCompareUser = toCompare.getUser();
+			if (this.call.equals(toCompareCall) && this.user.equals(toCompareUser))
+			{
+				result = true;
+			}
+		}
+		return result;
+	}
+}
