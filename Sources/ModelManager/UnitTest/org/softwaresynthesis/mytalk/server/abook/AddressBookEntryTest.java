666dcf48b2b2c3ed79f2de23a1726656b4c36ead
Diego Beraldin <diego.beraldin@gmail.com>
2013-05-02 21:06:39 +0200

    aggiustato test di Group

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/AddressBookEntryTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/abook/AddressBookEntryTest.java
@@ -1,10 +1,14 @@
 package org.softwaresynthesis.mytalk.server.abook;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Test dei metodi della classe {@link AddressBookEntry}
