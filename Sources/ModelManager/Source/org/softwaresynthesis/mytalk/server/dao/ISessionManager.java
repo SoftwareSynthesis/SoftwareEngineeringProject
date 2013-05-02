7ed6f9939957670f8fe5bb6fa7c45b2c2acbcdd2
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-05-02 16:33:14 +0200

    Gestore sessioni
    Creato il gestore delle sessioni di comunicazione con la base di dati
    del sistema MyTalk

---
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/dao/ISessionManager.java
@@ -0,0 +1,24 @@
+package org.softwaresynthesis.mytalk.server.dao;
+
+import org.hibernate.SessionFactory;
+
+/**
+ * Rappresenta il gestore delle sessioni
+ * di connessione verso la base di dati
+ * 
+ * @author 	Andrea Meneghinello
+ * @version	3.0
+ */
+public interface ISessionManager 
+{
+	/**
+	 * Restituisce una factory configurata
+	 * per la gestione di sessioni di connessione
+	 * con la base di dati del sistema MyTalk
+	 * 
+	 * @return 	{@link SessionFactory} factory per la
+	 * 			creazione di sessioni di connessione
+	 * 			con la base di dati
+	 */
+	public SessionFactory getSessionFacotry();
+}
