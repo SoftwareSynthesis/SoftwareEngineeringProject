4889e8bf0de8ea12474951dec7ca9f6b63527242
Diego Beraldin <diego.beraldin@gmail.com>
2013-05-04 09:54:15 +0200

    ultimati i test di server.authentication.security

---
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/security/EncodeTest.java
@@ -0,0 +1,58 @@
+package org.softwaresynthesis.mytalk.server.authentication.security;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Test della classe {@link Encode} avente il fine di verificare che la codifica
+ * dei dati avvenga come stabilito dall'algoritmo di crittografia AES a 128 bit
+ * con la chiave prescelta.
+ * 
+ * @author Diego Beraldin
+ * @version 2.0
+ */
+public class EncodeTest {
+	private static CryptoTemplate tester;
+	private static String plainText;
+	private static String cipherText;
+
+	/**
+	 * Inizializza l'oggetto da sottoporre a verifica e crea i dati necessari
+	 * all'esecuzione di tutti i test
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		// oggetto da testare
+		tester = new Encode();
+		// dati di test
+		plainText = "paperino";
+		cipherText = "Ub92LcoN/pLMLZnz6Bg4CA==";
+	}
+
+	/**
+	 * Verifica la correttezza dell'algoritmo di crittografia codificando una
+	 * stringa la cui versione cifrata è nota per la chiave prescelta e
+	 * confrontando la corrispondenza delle due stringhe.
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
+	@Test
+	public void testExecute() {
+		try {
+			String result = tester.execute(plainText);
+			assertNotNull(result);
+			assertEquals(cipherText, result);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
