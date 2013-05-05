2fcd0f4597d009a4c4cda921c36d4a2f6db3f3e6
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2013-05-05 10:51:56 +0200

    Controller per il login
    Corretto inizializzazione di un oggetto e creato il costruttore che
    inizializza la property per il corretto utilizzo di JAAS

--- Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/authentication/controller/LoginController.java
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/authentication/controller/LoginController.java
@@ -1,12 +1,11 @@
 package org.softwaresynthesis.mytalk.server.authentication.controller;
 
+import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.security.auth.login.LoginContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.softwaresynthesis.mytalk.server.AbstractController;
 import org.softwaresynthesis.mytalk.server.abook.IUserData;
 import org.softwaresynthesis.mytalk.server.authentication.CredentialLoader;
@@ -20,46 +19,72 @@ import org.softwaresynthesis.mytalk.server.dao.DataPersistanceManager;
  * @author 	Marco Schivo
  * @version 3.0
  */
-public class LoginController extends AbstractController{
-
-	@Override
+public class LoginController extends AbstractController
+{
 	/**
-	 * Esegue la richiesta di login per un utente ricevuta 
+	 * Inizializza il controller per eseguire
+	 * l'operazione di login
+	 */
+	public LoginController()
+	{
+		super();
+		String path = System.getenv("MyTalkConfiguration");
+		String separator = System.getProperty("file.separator");
+		path += separator + "MyTalk" + separator + "Conf" + separator + "LoginConfiguration.conf";
+		System.setProperty("java.security.auth.login.config", path);
+	}
+	
+	/**
+	 * Esegue la richiesta di login nel sistema MyTalk 
 	 * 
 	 * @author 	Marco Schivo
 	 * @version 3.0
 	 */
-	protected void doAction(HttpServletRequest request,	HttpServletResponse response) {
-		String email = null;
+	@Override
+	protected void doAction(HttpServletRequest request,	HttpServletResponse response) throws IOException
+	{
 		AESAlgorithm algorithm = null;
 		CredentialLoader loader = null;
-		LoginContext context = null;
-		DataPersistanceManager manager = null;
-		IUserData user = null;
+		DataPersistanceManager dao = null;
 		HttpSession session = null;
-		String result = null;
+		IUserData user = null;
+		LoginContext context = null;
 		PrintWriter writer = null;
-		
-		try{
-			email = request.getParameter("username");
+		String email = null;
+		String result = null;		
+		try
+		{
 			algorithm = new AESAlgorithm();
-			loader = new CredentialLoader(request);
+			loader = new CredentialLoader(request, algorithm);
 			context = new LoginContext("Configuration", loader);
-			context.login();			
-			manager = new DataPersistanceManager();
-			user = manager.getUserData(email);
-			if(user != null){
+			context.login();
+			dao = new DataPersistanceManager();
+			email = request.getParameter("username");
+			user = dao.getUserData(email);
+			if(user != null)
+			{
+				session = request.getSession(true);
 				session.setAttribute("context", context);
 				session.setAttribute("username", user.getMail());
+				this.setUserMail(user.getMail());
 				result = "{\"name\":\"" + user.getName() + "\"";
 				result += ", \"surname\":\"" + user.getSurname() + "\"";
 				result += ", \"email\":\"" + user.getMail() + "\"";
 				result += ", \"id\":\"" + user.getId() + "\"";
 				result += ", \"picturePath\":\"" + user.getPath() + "\"}";
 			}
+			else
+			{
+				result = "null";
+			}
 		}
 		catch (Exception ex)
-		{		
+		{
+			if (session != null)
+			{
+				session.invalidate();
+			}
+			result = "null";
 		}
 		finally
 		{
@@ -71,13 +96,13 @@ public class LoginController extends AbstractController{
 		}
 	}
 	
-	@Override
 	/**
 	 * Ridefinizione metodo check 
 	 * 
 	 * @author 	Marco Schivo
 	 * @version 3.0
 	 */
+	@Override
 	protected boolean check(HttpServletRequest request)
 	{
 		return true;
