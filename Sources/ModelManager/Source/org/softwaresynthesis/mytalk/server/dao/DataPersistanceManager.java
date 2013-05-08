66d3b4118d5c77fa49bfa8da25bfd1328069248b
Diego Beraldin <diego.beraldin@gmail.com>
2013-05-08 20:28:17 +0200

    eliminati commenti e reso semplice (e bello) getUserDatas(...) - closes 245

--- Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/dao/DataPersistanceManager.java
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/dao/DataPersistanceManager.java
@@ -217,19 +217,9 @@ public class DataPersistanceManager
 	public List<IUserData> getUserDatas(String mail, String name, String surname)
 	{
 		GetUtil select = this.factory.getUserDataUtil(this.manager);
-		//Iterator<IMyTalkObject> iterator = null;
-		//List<IMyTalkObject> collection = null;
 		List<IUserData> result = null;
 		String query = "from UserData as u where u.mail like '" + mail + "' or u.name like '" + name + "' or u.surname like '" + surname + "'";
-		//IUserData user = null;
 		result = (List)select.execute(query);
-		//iterator = collection.iterator();
-		//result = new ArrayList<IUserData>();
-		//while (iterator.hasNext())
-		//{
-			//user = (IUserData)iterator.next();
-			//result.add(user);
-		//}
 		return result;
 	}
 }
