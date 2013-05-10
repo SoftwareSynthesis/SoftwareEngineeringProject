47856c187a584a415648d3d65f5010e058100c21
Marco Schivo <marcoskivo@gmail.com>
2013-05-10 11:52:48 +0200

    GetMessagesController

---
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/message/controller/GetMessagesController.java
@@ -0,0 +1,78 @@
+package org.softwaresynthesis.mytalk.server.message.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.softwaresynthesis.mytalk.server.AbstractController;
+import org.softwaresynthesis.mytalk.server.abook.IUserData;
+import org.softwaresynthesis.mytalk.server.dao.DataPersistanceManager;
+import org.softwaresynthesis.mytalk.server.message.IMessage;
+
+public class GetMessagesController extends AbstractController{
+	/**
+	 * Recupera i messaggi nella
+	 * segreteria di un utente
+	 */
+	@Override
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws IOException 
+	{
+		DataPersistanceManager dao = null;
+		String result = null;
+		PrintWriter writer = null;
+		List<IMessage> messages = null;
+		Iterator<IMessage> messageIter = null;
+		IMessage message = null;
+		String separator = null;
+		String email = null;
+		IUserData user = null;
+		
+		try
+		{
+			dao = super.getDAOFactory();
+			email = super.getUserMail();
+			user = dao.getUserData(email);
+			messages = dao.getMessages(user);
+			if (messages != null)
+			{
+				messageIter = messages.iterator();
+				result = "[";
+				while (messageIter.hasNext() == true)
+				{
+					message = messageIter.next();
+					result += "{";
+					result += "\"id\":\"" + message.getId() + "\"";
+					result += ", \"sender\":\"" + message.getSender() + "\"";
+					result += ", \"status\":\"" + message.getNewer() + "\"";
+					result += ", \"video\":\"" + message.getVideo() + "\"";
+					result += ", \"date\":\"" + message.getDate() + "\"";
+					result += ", \"src\":\"Secretariat" + separator + message.getId() + ".wav\"";
+					if (messageIter.hasNext() == true)
+					{
+						result += "},";
+					}
+				}
+				result += "}]";
+			
+			}
+			else
+			{
+				result = "[]";
+			}
+			
+		}
+		catch (Exception ex)
+		{
+			result = "null";
+		}
+		finally
+		{
+			writer = response.getWriter();
+			writer.write(result);
+		}
+	}
+}
