17ee71b888aa15e871bee759056620d79efc77aa
Diego Beraldin <diego.beraldin@gmail.com>
2013-05-12 07:14:53 +0200

    corretti errori nel labirinto delle batterie di test ricorsive

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/AuthenticationTestSuite.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/authentication/AuthenticationTestSuite.java
@@ -5,12 +5,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.softwaresynthesis.mytalk.server.authentication.controller.*;
+import org.softwaresynthesis.mytalk.server.authentication.security.AESAlgorithmTest;
+import org.softwaresynthesis.mytalk.server.authentication.security.AESDecodeTest;
+import org.softwaresynthesis.mytalk.server.authentication.security.AESEncodeTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AuthenticationModuleTest.class, CredentialLoaderTest.class,
-		NameLoaderTest.class, PasswordLoaderTest.class,
-		PrincipalImplTest.class, LoginControllerTest.class,
-		LogoutControllerTest.class, QuestionControllerTest.class })
+@SuiteClasses({ AESAlgorithmTest.class, AESDecodeTest.class,
+		AESEncodeTest.class, AuthenticationModuleTest.class,
+		CredentialLoaderTest.class, NameLoaderTest.class,
+		PasswordLoaderTest.class, PrincipalImplTest.class,
+		LoginControllerTest.class, LogoutControllerTest.class,
+		QuestionControllerTest.class, RegisterControllerTest.class })
 public class AuthenticationTestSuite {
 
 }
