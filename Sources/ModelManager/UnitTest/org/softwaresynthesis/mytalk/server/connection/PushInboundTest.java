32fb41ca3b12a5719be3a8ce61489d2d5475d9f6
Diego Beraldin <diego.beraldin@gmail.com>
2013-05-18 15:27:05 +0200

    inizio bonifica di PushInboundTest

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/connection/PushInboundTest.java
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/server/connection/PushInboundTest.java
@@ -1,33 +1,111 @@
 package org.softwaresynthesis.mytalk.server.connection;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.softwaresynthesis.mytalk.server.ControllerManagerTest.getClients;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.util.Map;
 
-import org.junit.BeforeClass;
+import org.apache.catalina.websocket.WsOutbound;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.softwaresynthesis.mytalk.server.connection.PushInbound.State;
 
 /**
- * FIXME tutti questi test devono essere PROFONDAMENTE rivisitati!
+ * Verifica della classe {@link PushInbound}. Dinanzi a me non fuor cose create
+ * / se non etterne, e io etterno duro. / Lasciate ogne speranza, voi
+ * ch'intrate!
+ * 
+ * @author Diego Beraldin
+ * @version 2.0
  */
+@RunWith(MockitoJUnitRunner.class)
 public class PushInboundTest {
-	private static PushInbound tester;
+	private final Long id = 1L;
+	private final Long otherId = 2L;
+	private PushInbound tester;
+	Map<Long, PushInbound> clients;
+	@Mock
+	private ByteBuffer charBuffer;
+	@Mock
+	private CharBuffer buffer;
+	@Mock
+	private PushInbound other;
+	@Mock
+	private WsOutbound outbound;
+	private StringWriter writer;
 
-	@BeforeClass
-	public static void setUpBeforeClass() {
-		tester = new PushInbound();
+	/**
+	 * Reinizializza tutti gli elementi comuni a tutti i test e l'oggetto da
+	 * testare stesso, prima di tutte le verifiche qui contenute.
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
+	@Before
+	public void setUp() throws Exception {
+		writer = new StringWriter();
+		doAnswer(new Answer<Void>() {
+			@Override
+			public Void answer(InvocationOnMock invocation) {
+				try {
+					CharBuffer buf = (CharBuffer) invocation.getArguments()[0];
+					writer.write(buf.toString());
+				} catch (Exception e) {
+				}
+				return null;
+			}
+		}).when(outbound).writeTextMessage(any(CharBuffer.class));
+		clients = getClients();
+		tester = new PushInbound() {
+			@Override
+			WsOutbound getWsOutbound(PushInbound inbound) {
+				return outbound;
+			}
+		};
 	}
 
+	/**
+	 * Operazioni di clean-up necessarie dopo ogni test
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
+	@After
+	public void tearDown() {
+		clients.clear();
+	}
+
+	/**
+	 * TODO da documentare
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
 	@Test
 	public void testId() {
-		Long id = 1L;
 		tester.setId(id);
-		assertEquals(id, tester.getId());
+		Long result = tester.getId();
+		assertNotNull(result);
+		assertEquals(id, result);
 	}
 
+	/**
+	 * TODO da documentare
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
 	@Test
 	public void testState() {
 		State state = State.OCCUPIED;
@@ -35,141 +113,90 @@ public class PushInboundTest {
 		assertEquals(state, tester.getState());
 	}
 
+	/**
+	 * TODO da documentare
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
 	@Test(expected = IOException.class)
 	public void testOnBinaryMessageByteBuffer() throws IOException {
-		ByteBuffer buffer = mock(ByteBuffer.class);
-		tester.onBinaryMessage(buffer);
+		tester.onBinaryMessage(charBuffer);
 	}
 
-/* 
-	TODO da qui in poi è da bonificare tutto
-
+	/**
+	 * TODO da documentare
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
 	@Test
-	public void testConnectClient() {
-		// prepara i dati per il test
-		Long id = 1L;
-		// crea un finto messaggio
-		CharBuffer message = mock(CharBuffer.class);
-		when(message.toString()).thenReturn(
-				"[\"1\", \"" + id.toString() + "\"]");
-
-		try {
-			// invoca il metodo da testare
-			tester.onTextMessage(message);
-
-			// verifica che il client sia stato inserito correttamente
-			Map<Long, PushInbound> clients = ChannelServlet.getClients();
-			assertFalse(clients.isEmpty());
-			assertEquals(1, clients.size());
-		} catch (Throwable error) {
-			fail(error.getMessage());
-		} finally {
-			// azzera nuovamente l'array dei clients
-			ChannelServlet.setClients(new HashMap<Long, PushInbound>());
-		}
+	public void testConnectClient() throws Exception {
+		// messaggio di connessione
+		when(buffer.toString())
+				.thenReturn(String.format("[\"1\", \"%d\"]", id));
+		// invoca il metodo da testare
+		tester.onTextMessage(buffer);
+		// controlla il risultato
+		assertFalse(clients.isEmpty());
+		assertEquals(1, clients.size());
+		assertTrue(clients.containsKey(id));
 	}
 
+	/**
+	 * TODO da documentare
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
 	@Test
-	public void testDisconnectClient() {
-		// crea i dati per il test
-		Map<Long, PushInbound> clients = ChannelServlet.getClients();
-		Long id = 1L;
-		PushInbound channel = mock(PushInbound.class);
-		clients.put(id, channel);
-
-		// crea un finto messaggio
-		CharBuffer message = mock(CharBuffer.class);
-		when(message.toString()).thenReturn(
-				"[\"4\", \"" + id.toString() + "\"]");
-
-		try {
-			// invoca il metodo da testare
-			tester.onTextMessage(message);
-
-			// verifica la rimozione effettiva del canale
-			assertFalse(clients.containsKey(id));
-			assertTrue(clients.isEmpty());
-		} catch (Throwable error) {
-			fail(error.getMessage());
-		} finally {
-			// azzera nuovamente l'array dei clients
-			ChannelServlet.setClients(new HashMap<Long, PushInbound>());
-		}
+	public void testDisconnectClient() throws Exception {
+		// messaggio di disconnessione
+		when(buffer.toString())
+				.thenReturn(String.format("[\"4\", \"%d\"]", id));
+		// invoca il metodo da testare
+		tester.onTextMessage(buffer);
+		// controlla il risultato
+		assertFalse(clients.containsKey(id));
+		assertTrue(clients.isEmpty());
 	}
 
-	// FIXME questo test è pura e semplice fuffa
-	@Test(expected = NullPointerException.class)
-	public void testExchangeData() {
-		// crea i dati per il test
-		Map<Long, PushInbound> clients = ChannelServlet.getClients();
-		Long id = 1L;
-		PushInbound channel = new PushInbound();
-		clients.put(id, channel);
-
-		// crea un finto messaggio
-		CharBuffer message = mock(CharBuffer.class);
-		when(message.toString()).thenReturn(
-				"[\"2\", \"" + id.toString() + "\", \"dummy\"]");
-
-		try {
-			// invoca metodo da testare
-			tester.onTextMessage(message);
-		} catch (IOException ex) {
-			fail(ex.getMessage());
-		} finally {
-			// azzera nuovamente l'array dei clients
-			ChannelServlet.setClients(new HashMap<Long, PushInbound>());
-		}
+	/**
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
+	@Test
+	public void testExchangeData() throws Exception {
+		// prepara il messaggio e il finto 'peer' del client
+		clients.put(otherId, other);
+		when(buffer.toString()).thenReturn(
+				String.format("[\"2\", \"%d\", \"dummy\"]", id));
+		// invoca il metodo da testare
+		tester.onTextMessage(buffer);
+		// verifica l'output
+		writer.flush();
+		String message = writer.toString();
+		assertEquals("2|dummy", message);
 	}
-
-	// FIXME questo test è pura e semplice fuffa
-	@Test(expected = NullPointerException.class)
-	public void testSendId() {
-		// crea i dati per il test
-		Map<Long, PushInbound> clients = ChannelServlet.getClients();
-		Long id = 1L;
-		PushInbound channel = new PushInbound();
-		clients.put(id, channel);
-
-		// crea un finto messaggio
-		CharBuffer message = mock(CharBuffer.class);
-		when(message.toString()).thenReturn(
-				"[\"3\", \"" + id.toString() + "\"]");
-
-		try {
-			// invoca metodo da testare
-			tester.onTextMessage(message);
-		} catch (IOException ex) {
-			fail(ex.getMessage());
-		} finally {
-			// azzera nuovamente l'array dei clients
-			ChannelServlet.setClients(new HashMap<Long, PushInbound>());
-		}
+	
+	/**
+	 * 
+	 * @author Diego Beraldin
+	 * @version 2.0
+	 */
+	@Test
+	public void testSendIdToCallee() throws Exception {
+		// prepara il messaggio e il finto 'peer' del client
+		clients.put(otherId, other);
+		when(buffer.toString()).thenReturn(
+				String.format("[\"3\", \"%d\"]", id));
+		// invoca il metodo da testare
+		tester.setId(id);
+		tester.onTextMessage(buffer);
+		// verifica l'output
+		writer.flush();
+		String message = writer.toString();
+		assertEquals("3|" + id, message);
 	}
-
-	// FIXME questo test è pura e semplice fuffa
-	@Test(expected = NullPointerException.class)
-	public void testNotifyStateChange() {
-		// crea i dati per il test
-		Map<Long, PushInbound> clients = ChannelServlet.getClients();
-		Long id = 1L;
-		PushInbound channel = new PushInbound();
-		clients.put(id, channel);
-
-		// crea un finto messaggio
-		CharBuffer message = mock(CharBuffer.class);
-		when(message.toString()).thenReturn(
-				"[\"5\", \"" + id.toString() + "\", \"dummy\"]");
-
-		try {
-			// invoca metodo da testare
-			tester.onTextMessage(message);
-		} catch (IOException ex) {
-			fail(ex.getMessage());
-		} finally {
-		}
-		// azzera nuovamente l'array dei clients
-		ChannelServlet.setClients(new HashMap<Long, PushInbound>());
-	}
-*/
 }
