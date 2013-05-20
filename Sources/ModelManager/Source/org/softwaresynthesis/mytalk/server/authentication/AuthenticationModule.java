335ad8ac6b69397df5bdeb4bd24db0aba7ec488a
Diego Beraldin <diego.beraldin@gmail.com>
2013-05-20 10:56:21 +0200

    rifatto integralmente AuthenticationModuleTest

--- Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/authentication/AuthenticationModule.java
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/authentication/AuthenticationModule.java
@@ -13,8 +13,7 @@ import javax.security.auth.spi.LoginModule;
 import org.softwaresynthesis.mytalk.server.abook.IUserData;
 import org.softwaresynthesis.mytalk.server.dao.DataPersistanceManager;
 
-public final class AuthenticationModule implements LoginModule 
-{
+public class AuthenticationModule implements LoginModule {
 	private boolean login;
 	private boolean commit;
 	private CallbackHandler handler;
@@ -23,30 +22,22 @@ public final class AuthenticationModule implements LoginModule
 	private Subject subject;
 
 	/**
-	 * Termina la procedura di login cancellando
-	 * tutti i dati di eleborazione, comprese le 
-	 * credenziali utente
+	 * Termina la procedura di login cancellando tutti i dati di eleborazione,
+	 * comprese le credenziali utente
 	 * 
-	 * @throws 	{@link LoginException} se l'operazione non
-	 * 			dovesse andare a buone fine
+	 * @throws {@link LoginException} se l'operazione non dovesse andare a buone
+	 *         fine
 	 */
 	@Override
-	public boolean abort() throws LoginException 
-	{
-		if (this.login == false)
-		{
+	public boolean abort() throws LoginException {
+		if (this.isLoggedIn() == false) {
 			return false;
-		}
-		else
-		{
-			if (this.commit == true)
-			{
+		} else {
+			if (this.isCommitted() == true) {
 				this.login = false;
 				this.username = null;
 				this.principal = null;
-			}
-			else
-			{
+			} else {
 				this.logout();
 			}
 			return true;
@@ -54,20 +45,17 @@ public final class AuthenticationModule implements LoginModule
 	}
 
 	/**
-	 * Aggiunge le caratteristiche identificative al
-	 * {@link Subject} in modo che la sua identità
-	 * possa essere facilmente recuperata
+	 * Aggiunge le caratteristiche identificative al {@link Subject} in modo che
+	 * la sua identità possa essere facilmente recuperata
 	 * 
-	 * @throws 	{@link LoginException} se l'operazione
-	 * 			non dovesse andare a buon fine
+	 * @throws {@link LoginException} se l'operazione non dovesse andare a buon
+	 *         fine
 	 */
 	@Override
-	public boolean commit() throws LoginException
-	{
-		this.principal = new PrincipalImpl(this.username);
-		Set<Principal> principals = this.subject.getPrincipals();
-		if (principals.contains(this.principal) == false)
-		{
+	public boolean commit() throws LoginException {
+		this.principal = new PrincipalImpl(this.getUsername());
+		Set<Principal> principals = getPrincipals();
+		if (principals.contains(this.principal) == false) {
 			principals.add(this.principal);
 		}
 		this.username = null;
@@ -78,14 +66,19 @@ public final class AuthenticationModule implements LoginModule
 	/**
 	 * Inizializzazione del modulo di login
 	 * 
-	 * @param 	subject			{@link Subject} soggetto che deve essere autenticato
-	 * @param	handler			{@link CallbackHandler} handler per il caricamento delle credenziali
-	 * @param 	sharedState		{@link Map} mappa per un eventuale condivisione di stato
-	 * @param 	option			{@link Map} mappa per eventuali opzioni di login
+	 * @param subject
+	 *            {@link Subject} soggetto che deve essere autenticato
+	 * @param handler
+	 *            {@link CallbackHandler} handler per il caricamento delle
+	 *            credenziali
+	 * @param sharedState
+	 *            {@link Map} mappa per un eventuale condivisione di stato
+	 * @param option
+	 *            {@link Map} mappa per eventuali opzioni di login
 	 */
 	@Override
-	public void initialize(Subject subject, CallbackHandler handler, Map<String, ?> sharedState, Map<String, ?> option)
-	{
+	public void initialize(Subject subject, CallbackHandler handler,
+			Map<String, ?> sharedState, Map<String, ?> option) {
 		this.login = false;
 		this.commit = false;
 		this.handler = handler;
@@ -95,57 +88,43 @@ public final class AuthenticationModule implements LoginModule
 	}
 
 	/**
-	 * Effettua l'autenticazione di un utente
-	 * nel sistema MyTalk
+	 * Effettua l'autenticazione di un utente nel sistema MyTalk
 	 * 
-	 * @throws 	{@link LoginException} se l'operazione non
-	 * 			dovesse andarea a buon fine
+	 * @throws {@link LoginException} se l'operazione non dovesse andarea a buon
+	 *         fine
 	 */
 	@Override
-	public boolean login() throws LoginException 
-	{
+	public boolean login() throws LoginException {
 		DataPersistanceManager dao;
 		Loader[] callbacks = null;
 		IUserData user = null;
 		String toComparePassword = null;
-		if (this.handler != null)
-		{
+		if (this.handler != null) {
 			callbacks = new Loader[2];
-			callbacks[0] = new NameLoader();
-			callbacks[1] = new PasswordLoader();
-			try
-			{
+			callbacks[0] = createNameLoader();
+			callbacks[1] = createPasswordLoader();
+			try {
 				this.handler.handle(callbacks);
-			}
-			catch (IOException ex)
-			{
+			} catch (IOException ex) {
 				throw new LoginException(ex.getMessage());
-			}
-			catch (UnsupportedCallbackException ex)
-			{
+			} catch (UnsupportedCallbackException ex) {
 				throw new LoginException(ex.getMessage());
 			}
 		}
 		this.username = callbacks[0].getData();
-		dao = new DataPersistanceManager();
+		dao = getDAOFactory();
 		user = dao.getUserData(username);
-		if (user != null)
-		{
+		if (user != null) {
 			toComparePassword = user.getPassword();
-			if (toComparePassword.equals(callbacks[1].getData()))
-			{
+			if (toComparePassword.equals(callbacks[1].getData())) {
 				this.login = true;
 				return true;
-			}
-			else
-			{
+			} else {
 				this.login = false;
 				this.username = null;
 				throw new FailedLoginException("Password errata");
 			}
-		}
-		else
-		{
+		} else {
 			throw new FailedLoginException("Username errato");
 		}
 	}
@@ -153,17 +132,52 @@ public final class AuthenticationModule implements LoginModule
 	/**
 	 * Effettua il logout dal sistema MyTalk
 	 * 
-	 * @throws	{@link LoginException} se l'operazione non
-	 * 			dovesse andare a buon fine
+	 * @throws {@link LoginException} se l'operazione non dovesse andare a buon
+	 *         fine
 	 */
 	@Override
-	public boolean logout() throws LoginException 
-	{
-		Set<Principal> principals = this.subject.getPrincipals();
-		principals.remove(this.principal);
+	public boolean logout() throws LoginException {
+		Set<Principal> principals = getPrincipals();
+		principals.remove(getPrincipal());
 		this.login = false;
 		this.commit = false;
 		this.username = null;
 		return true;
+	}
+
+	/*
+	 * Mi dispiace ma queste mi servono assolutamente per i test! Ad ogni modo,
+	 * spero converrete che hanno un aspetto del tutto innocuo...
+	 */
+	DataPersistanceManager getDAOFactory() {
+		return new DataPersistanceManager();
+	}
+
+	boolean isLoggedIn() {
+		return login;
+	}
+
+	boolean isCommitted() {
+		return commit;
+	}
+
+	Loader createNameLoader() {
+		return new NameLoader();
+	}
+
+	Loader createPasswordLoader() {
+		return new PasswordLoader();
+	}
+
+	Set<Principal> getPrincipals() {
+		return subject.getPrincipals();
+	}
+
+	String getUsername() {
+		return username;
+	}
+
+	Principal getPrincipal() {
+		return principal;
 	}
 }
