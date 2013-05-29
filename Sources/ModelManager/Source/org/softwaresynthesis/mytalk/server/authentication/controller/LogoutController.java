2c95e92aa4118f2c44589c9f9eb86f8aa82bd376
Marco Schivo <marcoskivo@gmail.com>
2013-05-29 15:37:52 +0200

    Ora funzionanti tutti i controller di authe&abook
    spero che diego non mi uccida per le modifiche fatte ai controller!

--- Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/authentication/controller/LogoutController.java
+++ Sources/ModelManager/Source/org/softwaresynthesis/mytalk/server/authentication/controller/LogoutController.java
@@ -2,11 +2,20 @@ package org.softwaresynthesis.mytalk.server.authentication.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.CharBuffer;
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.security.auth.login.LoginContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.softwaresynthesis.mytalk.server.AbstractController;
+import org.softwaresynthesis.mytalk.server.ControllerManager;
+import org.softwaresynthesis.mytalk.server.abook.IAddressBookEntry;
+import org.softwaresynthesis.mytalk.server.abook.IUserData;
+import org.softwaresynthesis.mytalk.server.connection.PushInbound;
+import org.softwaresynthesis.mytalk.server.dao.DataPersistanceManager;
 
 /**
  * Controller che gestisce il logout
@@ -28,8 +37,36 @@ public final class LogoutController extends AbstractController
 		LoginContext context = null;
 		PrintWriter writer = null;
 		String result = null;
+		Long id = null;
+		String email = null;
+		PushInbound client = null;
+		IUserData user = null;
+		DataPersistanceManager dao = null;
+		Set<IAddressBookEntry> rubrica = null;
+		Iterator<IAddressBookEntry> iterator = null;
+		IAddressBookEntry entry = null;
+		PushInbound pi = null;
 		try
 		{
+			dao = new DataPersistanceManager();
+			email = getUserMail();
+			user = dao.getUserData(email);
+			id = user.getId();
+			rubrica = user.getAddressBook();
+			iterator = rubrica.iterator();
+			while (iterator.hasNext())
+			{
+				entry = iterator.next();
+				pi = ControllerManager.findClient(entry.getContact().getId());
+				if (pi != null)
+				{
+					String msg = "5|" + id + "|offline";
+					pi.getWsOutbound().writeTextMessage(CharBuffer.wrap(msg));
+				}
+			}
+			client = ControllerManager.findClient(id);
+			if (client != null)
+				ControllerManager.removeClient(client);
 			session = request.getSession(false);
 			context = (LoginContext)session.getAttribute("context");
 			context.logout();
