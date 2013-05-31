98224754dcd8406e2f351ff87fb83c6df5518455
Stefano Farronato <stefar187@gmail.com>
2013-05-31 16:13:35 +0200

    MessagePresenterTest (ancora non funzionante)

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/MessagePresenterTest.js
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/MessagePresenterTest.js
@@ -1,56 +1,48 @@
-module ("MessagePanelPresenterTest", {
-    setup:
-        function() {
-			communicationcenter = new Object();
-			communicationcenter.my = {id:"01"};
-			configurationFile = "/ModelManager/WebContent/Conf/servletlocationtest.xml";
-			test = new MessagePanelPresenter("MessageManager.php");								},
-	teardown: 
-		function(){}
-	}
-);
-
-
-test("createPanelTest()",
-    function () {
+module(
+		"MessagePanelPresenterTest",
+		{
+			setup : function() {
+					// stub di communicationcenter
+				communicationcenter = new Object();
+				communicationcenter.my = { id:"01"};
+				commandURL = "http://localhost:8888/ModelManager/WebContent/Conf/controllerManagerStub.php";
+				// stub di mediator
+				mediator = {
+					getView : function(someString) {
+						var viewRequest = new XMLHttpRequest();
+						viewRequest.open("POST",
+								"clientview/MessageView.html", false);
+						viewRequest.send();
+						var div = document.createElement("div");
+						div.innerHTML = viewRequest.responseText;
+						if (document.getElementById("MessagePanel") == null) {
+							document.body.appendChild(div);
+							div.style.display = "none";
+						}
+					},
+					
+					getContactById: function(someString){
+						return "01";
+					},
+					createNameLabel: function(someString){
+						return "pippo";
+					}
+				};
+				// oggetto da testare
+				tester = new MessagePanelPresenter();
+			},
+			teardown : function() {
+			}
+		});
+		
+test("testDisplay()",
+	function() {
 		var i = 0;
-		// creo l'elemento 'MessagePanel'
-		var element = test.createPanel();
-		// estraggo la lista dei figli di questo pannello
-	var list = element.childNodes;
-		// controllo che abbia esattamente due figli
-		equal(list.length,2,"il numero di figli dell'elemento restituito e' 2");
+		var event = new CustomEvent("showMessagePanel");
+		document.dispatchEvent(event);
+		tester.display();
+		var name = document.getElementById("MessagePanel").innerHTML;
+		equal(name, "01", "id impostato correttamente");
 		i++;
-		
-		// controllo che il primo figlio sia il video
-		equal(list[0].nodeName, "VIDEO", "il primo figlio dell'elemento e' un video");
-		i++;
-		
-		// controllo che il secondo figlio sia un div
-		equal(list[1].nodeName, "DIV", "il primo figlio dell'elemento e' un div");
-		i++;
-		
-
-	
-		// estraggo la lista dei figli di video
-		var items = list[0].childNodes;
-		equal(items.length, 1, "il video ha un figlio");
-		i++;
-		//controllo che il figlio di video sia source
-		equal(items[0].nodeName, "SOURCE", "il figlio di video e' source ");
-		i++;
-		
-		
-		// estraggo la lista dei figli del di div
-	var items_uno = list[1].childNodes;
-	equal(items_uno.length, 1, "il div ha un figlio");
-	i++;
-		//controllo che il figlio di div sia ul
-	equal(items_uno[0].nodeName, "UL", "il figlio di video e' ul ");
-		i++;
-		
-		
-		
 		expect(i);
-    });
-
+	});
