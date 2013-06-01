d954c742cdf14a155e49d44326b70fc67b4e57a0
Diego Beraldin <diego.beraldin@gmail.com>
2013-06-01 10:31:54 +0200

    portato all'89% il coverage di ToolsPresenter

--- Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/ToolsPresenterTest.js
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/guicontrol/ToolsPresenterTest.js
@@ -1,3 +1,6 @@
+/**
+ * Verifica di ToolsPresenter
+ */
 module("ToolsPanelPresenter", {
 	setup : function() {
 		// stub di mediator
@@ -8,7 +11,14 @@ module("ToolsPanelPresenter", {
 				viewRequest.send();
 				var div = document.createElement("div");
 				div.innerHTML = viewRequest.responseText;
+				document.dispatchEvent(new CustomEvent("eventRaised"));
 				return div.childNodes[0];
+			}
+		};
+		// stub di communicationcenter
+		communicationcenter = {
+			disconnect : function() {
+				document.dispatchEvent(new CustomEvent("eventRaised"));
 			}
 		};
 		// inizializza l'oggetto da testare
@@ -24,101 +34,234 @@ module("ToolsPanelPresenter", {
 	}
 });
 
-test("testDestroy()", function() {
-	var i = 0;
-	tester.destroy();
-	var element = document.getElementById("ToolsPanel");
-	equal(element, null, "pannello rimosso");
-	i++;
-	expect(i);
-});
-
+/**
+ * Verifica la creazione e l'inizializzazione del pannello
+ */
 test("testInitialize()", function() {
 	var i = 0;
 	tester.initialize(mediator.getView("tools"));
 	var element = document.getElementById("ToolsPanel");
 	element.style.display = "none";
 
-	equal(element.nodeName, "DIV", "elemento del tipo corretto");
+	equal(element.nodeName, "DIV");
 	i++;
 
-	var children = element.childNodes;
-	equal(children.length, 7, "numero di figli corretto");
+	var children = element.children;
+	equal(children.length, 3);
 	i++;
 
 	var child = document.getElementsByClassName("panelHeader")[0];
-	equal(child.nodeName, "DIV", "corretto");
+	equal(child.nodeName, "DIV");
 	i++;
 
 	child = document.getElementById("selectState");
-	equal(child.nodeName, "SELECT", "corretto");
+	equal(child.nodeName, "SELECT");
 	i++;
 
 	child = document.getElementById("ToolsList");
-	equal(child.nodeName, "UL", "corretto");
+	equal(child.nodeName, "UL");
 	i++;
 
 	child = document.getElementById("liAnswering");
-	equal(child.nodeName, "LI", "corretto");
+	equal(child.nodeName, "LI");
 	i++;
-	equal(child.innerHTML, "Segreteria", "testo corretto");
+	equal(child.innerHTML, "Segreteria");
 	i++;
 
 	child = document.getElementById("liSetting");
-	equal(child.nodeName, "LI", "corretto");
+	equal(child.nodeName, "LI");
 	i++;
-	equal(child.innerHTML, "Impostazioni", "testo corretto");
+	equal(child.innerHTML, "Impostazioni");
 	i++;
 
 	child = document.getElementById("liCallList");
-	equal(child.nodeName, "LI", "corretto");
+	equal(child.nodeName, "LI");
 	i++;
-	equal(child.innerHTML, "Lista chiamate", "testo corretto");
+	equal(child.innerHTML, "Lista chiamate");
 	i++;
 
 	child = document.getElementById("liGroup");
-	equal(child.nodeName, "LI", "corretto");
+	equal(child.nodeName, "LI");
 	i++;
-	equal(child.innerHTML, "Gruppi", "testo corretto");
+	equal(child.innerHTML, "Gruppi");
 	i++;
 
 	child = document.getElementById("liSearch");
-	equal(child.nodeName, "LI", "corretto");
+	equal(child.nodeName, "LI");
 	i++;
-	equal(child.innerHTML, "Ricerca", "testo corretto");
+	equal(child.innerHTML, "Ricerca");
 	i++;
 
 	child = document.getElementById("liLogout");
-	equal(child.nodeName, "LI", "corretto");
+	equal(child.nodeName, "LI");
 	i++;
-	equal(child.innerHTML, "Logout", "testo corretto");
+	equal(child.innerHTML, "Logout");
 	i++;
 
 	expect(i);
 });
 
+/**
+ * Verifica la distruzione del pannello
+ */
+test("testDestroy()", function() {
+	var i = 0;
+	tester.initialize(mediator.getView("tools"));
+	tester.destroy();
+	var element = document.getElementById("ToolsPanel");
+	equal(element, null);
+	i++;
+	expect(i);
+});
+
+/**
+ * Verifica che sia possibile aggiungere il pulsante 'Comunicazione'
+ */
 test("testAddCommunication()", function() {
 	var i = 0;
 	tester.initialize(mediator.getView("tools"));
 	tester.addCommunicationFunction();
 
 	var element = document.getElementById("CallFunction");
-	equal(element.nodeName, "LI", "corretto");
+	equal(element.nodeName, "LI");
 	i++;
-	equal(element.innerHTML, "Chiamata", "testo corretto");
+	equal(element.innerHTML, "Chiamata");
 	i++;
 
 	expect(i);
 });
 
+/**
+ * Verifica che sia possibile rimuovere il pulsante 'Comunicazione'
+ */
 test("testRemoveCommunication()", function() {
 	var i = 0;
 	tester.initialize(mediator.getView("tools"));
+	tester.addCommunicationFunction();
 	tester.removeCommunicationFunction();
 
 	var element = document.getElementById("CallFunction");
-	equal(element, null, "rimossa funzione correttamente");
+	equal(element, null);
 	i++;
 
 	expect(i);
+});
+
+/**
+ * Verifica la possibilità di effettuare logout tramite evento
+ */
+test("testOnLogout()", function() {
+	var bool = false;
+	document.addEventListener("eventRaised", function() {
+		bool = true;
+	});
+
+	document.dispatchEvent(new CustomEvent("logout"));
+	ok(bool);
+});
+
+/**
+ * Verifica il comportamento al cambiamento di stato
+ */
+test("testUpdateStateValue()", function() {
+	var i = 0;
+	changeMyState = new CustomEvent("changeMyState");
+	tester.initialize(mediator.getView("tools"));
+	var element = document.getElementById("selectState");
+	element.selectedIndex = 0;
+
+	element = tester.updateStateValue();
+	equal(element.nodeName, "SELECT");
+	i++;
+	equal(element.children.length, 2);
+	i++;
+
+	expect(i);
+});
+
+/**
+ * Verifica che il presenter risponda all'evento showToolsPanel
+ */
+test("testOnShowToolsPanel()", function() {
+	var bool = false;
+	document.addEventListener("eventRaised", function() {
+		bool = true;
+	});
+	document.dispatchEvent(new CustomEvent("showToolsPanel"));
+	ok(bool);
+});
+
+/**
+ * Verifica il funzionamento del pulsante di ricerca
+ */
+test("testPerformSearch", function() {
+	showSearchResultPanel = new CustomEvent("showSearchResultPanel");
+	tester.initialize(mediator.getView("tools"));
+	var bool = false;
+	document.addEventListener("showSearchResultPanel", function() {
+		bool = true;
+	});
+	var liSearch = document.getElementById("liSearch");
+	liSearch.dispatchEvent(new MouseEvent("click"));
+	ok(bool);
+});
+
+/**
+ * Verifica il funzionamento del pulsante per i gruppi
+ */
+test("testPerformSearch", function() {
+	showGroupPanel = new CustomEvent("showGroupPanel");
+	tester.initialize(mediator.getView("tools"));
+	var bool = false;
+	document.addEventListener("showGroupPanel", function() {
+		bool = true;
+	});
+	var liSearch = document.getElementById("liGroup");
+	liSearch.dispatchEvent(new MouseEvent("click"));
+	ok(bool);
+});
+
+/**
+ * Verifica il funzionamento del pulsante per lo storico chiamate
+ */
+test("testPerformSearch", function() {
+	showCallHistoryPanel = new CustomEvent("showCallHistoryPanel");
+	tester.initialize(mediator.getView("tools"));
+	var bool = false;
+	document.addEventListener("showCallHistoryPanel", function() {
+		bool = true;
+	});
+	var liSearch = document.getElementById("liCallList");
+	liSearch.dispatchEvent(new MouseEvent("click"));
+	ok(bool);
+});
+
+/**
+ * Verifica il funzionamento del pulsante per le impostazioni utente
+ */
+test("testPerformSearch", function() {
+	showAccountSettingPanel = new CustomEvent("showAccountSettingPanel");
+	tester.initialize(mediator.getView("tools"));
+	var bool = false;
+	document.addEventListener("showAccountSettingPanel", function() {
+		bool = true;
+	});
+	var liSearch = document.getElementById("liSetting");
+	liSearch.dispatchEvent(new MouseEvent("click"));
+	ok(bool);
+});
+
+/**
+ * Verifica il funzionamento del pulsante per la segreteria
+ */
+test("testPerformSearch", function() {
+	showMessagePanel = new CustomEvent("showMessagePanel");
+	tester.initialize(mediator.getView("tools"));
+	var bool = false;
+	document.addEventListener("showMessagePanel", function() {
+		bool = true;
+	});
+	var liSearch = document.getElementById("liAnswering");
+	liSearch.dispatchEvent(new MouseEvent("click"));
+	ok(bool);
 });
