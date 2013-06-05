a62130dd75882cbe89b3790395565157e35c5f3f
Diego Beraldin <diego.beraldin@gmail.com>
2013-06-05 17:09:25 +0200

    inizio test del monolite

---
+++ Sources/ModelManager/UnitTest/org/softwaresynthesis/mytalk/clientpresenter/kernel/CommunicationCenterTest.js
@@ -0,0 +1,26 @@
+/**
+ * Verifica del Monolite
+ * 
+ * @author Diego Beraldin
+ * @version 2.0
+ */
+module("CommunicationCenterTest", {
+	setup : function() {
+		// oggetto da testare
+		monolith = new CommunicationCenter();
+	},
+	teardown : function() {
+	}
+});
+
+/**
+ * Verifica il comportamento del metodo connect
+ * 
+ * @author Diego Beraldin
+ * @version 2.0
+ */
+test("testConnect()", function() {
+	var i = 0;
+	
+	expect(i);
+});
