c1599ee6b91e7c6181cbe6a8cd5ea6edab76bc34
Diego Beraldin <diego.beraldin@gmail.com>
2013-06-12 13:44:04 +0200

    aggiustati test del server per la n-esima volta con n->infinity

--- Sources/ModelManager/Test/org/softwaresynthesis/mytalk/server/dao/DataPersistanceManagerTest.java
+++ Sources/ModelManager/Test/org/softwaresynthesis/mytalk/server/dao/DataPersistanceManagerTest.java
@@ -914,8 +914,8 @@ public class DataPersistanceManagerTest {
 		String name = "paperino";
 		String surname = "de paperoni";
 		String query = "from UserData as u where u.mail like '%" + mail
-				+ "%' or u.name like '" + name + "' or u.surname like '"
-				+ surname + "'";
+				+ "%' or u.name like '%" + name + "%' or u.surname like '%"
+				+ surname + "%'";
 		// configura il comportamento dei mock
 		when(getter.execute(query)).thenReturn(list);
 
